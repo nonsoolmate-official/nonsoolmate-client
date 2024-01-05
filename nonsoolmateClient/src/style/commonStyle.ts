8fc4d4e364017c4a9de51cf1da95d0521e543fcc
HAE2NI <hae0_02ni@ewhain.net>
2024-01-05 15:17:15 +0900

    [ design ] commonStyle

---
+++ nonsoolmateClient/src/style/commonStyle.ts
@@ -0,0 +1,36 @@
+import styled, { css } from "styled-components";
+
+export const commonFlex = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const columnFlex = css`
+  ${commonFlex};
+
+  flex-direction: column;
+`;
+
+export const mainButtonStyle = styled.button`
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.main_blue};
+  font: ${({ theme }) => theme.fonts.Headline5};
+  color: ${({ theme }) => theme.colors.white};
+  ${commonFlex}
+`;
+
+export const lightBlueButtonStyle = styled.button`
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.light_blue};
+  font: ${({ theme }) => theme.fonts.Headline5};
+  color: ${({ theme }) => theme.colors.main_blue};
+  ${commonFlex}
+`;
+
+export const subButtonStyle = styled.button`
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.colors.light_blue};
+  color: ${({ theme }) => theme.colors.main_blue};
+  ${commonFlex}
+`;
