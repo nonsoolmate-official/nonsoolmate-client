68d9a1542fa92b70d830ba2233fbecbb9febad94
HAE2NI <hae0_02ni@ewhain.net>
2024-01-05 15:40:41 +0900

    [ etc ] vite.config.ts에 svgr() 추가

--- nonsoolmateClient/vite.config.ts
+++ nonsoolmateClient/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "src") },
