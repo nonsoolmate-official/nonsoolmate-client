ea8f919cdf3cfd128ee2a7cf4ff4d23f775f19b8
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-06 23:14:22 +0900

    [ feat/#3 ] 라우터 outlet 구현

--- nonsoolmateClient/src/router.tsx
+++ nonsoolmateClient/src/router.tsx
@@ -2,12 +2,27 @@ import Error from "error";
 import Main from "@pages/Main";
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "layout/RootLayout";
+import Home from "home";
+import HomePractice from "home/components/HomePractice";
+import HomeStudy from "home/components/HomeStudy";
+import HomeTest from "home/components/HomeTest";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     errorElement: <Error />,
-    children: [{ path: "/example", element: <Main /> }],
+    children: [
+      { path: "/example", element: <Main /> },
+      {
+        path: "/home",
+        element: <Home />,
+        children: [
+          { path: "/home/practice", element: <HomePractice /> },
+          { path: "/home/study", element: <HomeStudy /> },
+          { path: "/home/test", element: <HomeTest /> },
+        ],
+      },
+    ],
   },
 ]);
