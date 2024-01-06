8803fe6f0c7f86ee75d5eebad025565c604f1d0e
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-06 23:16:52 +0900

    [ feat/#3 ] Home페이지 outlet에서 쓰일 컴포넌트

---
+++ nonsoolmateClient/src/home/components/HomeTest.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import HomeTestSet from "./HomeTestSet";
+import HomeTestUnset from "./HomeTestUnset";
+
+export default function HomeTest() {
+  return (
+    <>
+      {/* {none&&<HomeTestUnset/>}
+      {!none&&<HomeTestSet/>} */}
+    </>
+  );
+}
+
