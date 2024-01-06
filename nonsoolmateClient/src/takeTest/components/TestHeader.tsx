0364f40e3c0e77e7c15da6f81d7bf8dedf1c992c
Soyoung Park <soyoung008@naver.com>
2024-01-06 23:02:32 +0900

    [ design/#11 ] 헤더 고정 및 배경색 추가

--- nonsoolmateClient/src/takeTest/components/TestHeader.tsx
+++ nonsoolmateClient/src/takeTest/components/TestHeader.tsx
@@ -21,7 +21,10 @@ const TestHeaderContainer = styled.header`
   ${commonFlex};
 
   justify-content: space-between;
+  position: sticky;
+  top: 0;
   padding: 1.7rem 3.6rem 1.3rem;
+  background-color: ${({ theme }) => theme.colors.white};
   box-shadow: 0 0 1.2rem 0 ${({ theme }) => theme.colors.grey_200};
 `;
 const TestCloseButton = styled(mainButtonStyle)`
