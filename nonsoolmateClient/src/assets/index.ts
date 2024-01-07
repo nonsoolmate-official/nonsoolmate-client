6c9207b4996486085097b148eae835db44fdb7af
Soyoung Park <soyoung008@naver.com>
2024-01-07 19:35:33 +0900

    [ assets/#16 ] 아이콘 추가

--- nonsoolmateClient/src/assets/index.ts
+++ nonsoolmateClient/src/assets/index.ts
@@ -6,15 +6,19 @@ import DownArrowIc from "./icon/downArrowIc.svg?react";
 import LeftArrowBigIc from "./icon/leftArrowBigIc.svg?react";
 import LeftArrowBlackBtn from "./icon/leftArrowBlackBtn.svg?react";
 import LeftArrowWhiteIc from "./icon/leftArrowWhiteIc.svg?react";
+import LeftCircleIc from "./icon/leftCircleIc.svg?react";
+import LeftCircleLongIc from "./icon/leftCircleLongIc.svg?react";
 
 import PrecautionNumIc from "./icon/precautionNumIc.svg?react";
 
 import RightArrowBigIc from "./icon/rightArrowBigIc.svg?react";
 import RightArrowIc from "./icon/rightArrowIc.svg?react";
 import RightArrowWhiteIc from "./icon/rightArrowWhiteIc.svg?react";
+import RightCircleIc from "./icon/rightCircleIc.svg?react";
 
 import UpArrowBoldIc from "./icon/upArrowBoldIc.svg?react";
 import UpArrowIc from "./icon/upArrowIc.svg?react";
+import UpCircleIc from "./icon/upCircleIc.svg?react";
 
 export {
   BackToListIc,
@@ -23,10 +27,14 @@ export {
   LeftArrowBigIc,
   LeftArrowBlackBtn,
   LeftArrowWhiteIc,
+  LeftCircleIc,
+  LeftCircleLongIc,
   PrecautionNumIc,
   RightArrowBigIc,
   RightArrowIc,
   RightArrowWhiteIc,
+  RightCircleIc,
   UpArrowBoldIc,
   UpArrowIc,
+  UpCircleIc,
 };
