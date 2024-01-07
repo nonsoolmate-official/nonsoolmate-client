7c5bb3f6c90e222f23fac6898c2252160689960b
Soyoung Park <soyoung008@naver.com>
2024-01-07 13:27:12 +0900

    [ design/#14 ] 임시로 시험지 이미지 배치

--- nonsoolmateClient/src/takeTest/components/TestSection.tsx
+++ nonsoolmateClient/src/takeTest/components/TestSection.tsx
@@ -1,12 +1,14 @@
 import { LeftArrowBigIc, RightArrowBigIc } from "@assets/index";
 import styled from "styled-components";
-
+import testPaper1 from "../../assets/image/testPaperImg1.jpeg";
+import { commonFlex } from "style/commonStyle";
 export default function TestSection() {
   return (
     <TestSectionContainer>
       <PreviousPageButton>
         <LeftArrowBigIcon />
       </PreviousPageButton>
+      <TestPaperImage src={testPaper1} alt="시험지" />
       <NextPageButton>
         <RightArrowBigIcon />
       </NextPageButton>
@@ -14,6 +16,8 @@ export default function TestSection() {
   );
 }
 const TestSectionContainer = styled.section`
+  ${commonFlex};
+
   background-color: ${({ theme }) => theme.colors.grey_50};
 `;
 const PreviousPageButton = styled.button`
@@ -35,4 +39,9 @@ const RightArrowBigIcon = styled(RightArrowBigIc)`
 const LeftArrowBigIcon = styled(LeftArrowBigIc)`
   width: 5.6rem;
   height: 5.6rem;
+`;
+const TestPaperImage = styled.img`
+  width: 93.6rem;
+  height: 132.38rem;
+  margin: 2.4rem 8rem;
 `;
