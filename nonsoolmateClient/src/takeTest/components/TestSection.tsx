8d781fe37e1c50443c56481d840598cc183b20a0
Soyoung Park <soyoung008@naver.com>
2024-01-07 12:50:33 +0900

    [ design/#14 ] TestSection 아이콘 및 배경색 설정

--- nonsoolmateClient/src/takeTest/components/TestSection.tsx
+++ nonsoolmateClient/src/takeTest/components/TestSection.tsx
@@ -1,3 +1,38 @@
+import { LeftArrowBigIc, RightArrowBigIc } from "@assets/index";
+import styled from "styled-components";
+
 export default function TestSection() {
-  return <div>TestSection</div>;
+  return (
+    <TestSectionContainer>
+      <PreviousPageButton>
+        <LeftArrowBigIcon />
+      </PreviousPageButton>
+      <NextPageButton>
+        <RightArrowBigIcon />
+      </NextPageButton>
+    </TestSectionContainer>
+  );
 }
+const TestSectionContainer = styled.section`
+  background-color: ${({ theme }) => theme.colors.grey_50};
+`;
+const PreviousPageButton = styled.button`
+  position: fixed;
+  top: 50%;
+  left: 8rem;
+  padding: 0;
+`;
+const NextPageButton = styled.button`
+  position: fixed;
+  top: 50%;
+  right: 8rem;
+  padding: 0;
+`;
+const RightArrowBigIcon = styled(RightArrowBigIc)`
+  width: 5.6rem;
+  height: 5.6rem;
+`;
+const LeftArrowBigIcon = styled(LeftArrowBigIc)`
+  width: 5.6rem;
+  height: 5.6rem;
+`;
--- nonsoolmateClient/src/takeTest/index.tsx
+++ nonsoolmateClient/src/takeTest/index.tsx
@@ -1,5 +1,11 @@
 import TestHeader from "./components/TestHeader";
+import TestSection from "./components/TestSection";
 
 export default function index() {
-  return <TestHeader />;
+  return (
+    <>
+      <TestHeader />
+      <TestSection />
+    </>
+  );
 }
