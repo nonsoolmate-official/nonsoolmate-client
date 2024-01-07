68eeb4405650059ea911af606da03d7775f75d2b
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-07 23:32:41 +0900

    [ etc/#17 ] index.tsx에 라우터 주석 추가

--- nonsoolmateClient/src/home/index.tsx
+++ nonsoolmateClient/src/home/index.tsx
@@ -3,6 +3,17 @@ import styled from "styled-components";
 import HomeHeader from "./components/HomeHeader";
 import HomeSide from "./components/HomeSide";
 
+// outlet이 적응되는대로 주석 얼른 지울겠습니다 ㅠ
+// {
+//   path: "/home",
+//   element: <Home />,
+//   children: [
+//     { path: "/home/practice", element: <HomePractice /> },
+//     { path: "/home/study", element: <HomeStudy /> },
+//     { path: "/home/test", element: <HomeTest /> },
+//   ],
+// }
+
 export default function index() {
   return (
     <>
