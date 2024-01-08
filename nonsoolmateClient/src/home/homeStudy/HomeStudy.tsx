7b7950ed5e4f16bc644b373da2708d6414fdcd6a
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-09 02:10:54 +0900

    [ design/#27 ] 100vh의 자식 컴포넌트에 height 100%

--- nonsoolmateClient/src/home/homeStudy/HomeStudy.tsx
+++ nonsoolmateClient/src/home/homeStudy/HomeStudy.tsx
@@ -18,6 +18,7 @@ const Container = styled.section`
   flex-direction: column;
   gap: 0.7rem;
   width: 93.5rem;
+  height: 100%;
   padding: 6.7rem 21.5rem 0 2.4rem;
   background-color: ${({ theme }) => theme.colors.grey_50};
 `;
