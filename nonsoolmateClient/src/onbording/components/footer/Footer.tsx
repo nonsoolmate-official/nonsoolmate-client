ab2dbff642fc1a6bf5bb0cb5bf432b374d57b76e
HAE2NI <hae0_02ni@ewhain.net>
2024-01-08 21:58:11 +0900

    [ style ] footer 구조도 잡기

--- nonsoolmateClient/src/onbording/components/footer/Footer.tsx
+++ nonsoolmateClient/src/onbording/components/footer/Footer.tsx
@@ -1,3 +1,11 @@
+import FooterLeft from "./FooterLeft";
+import FooterRight from "./FooterRight";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <FooterLeft />
+      <FooterRight />
+    </>
+  );
 }
