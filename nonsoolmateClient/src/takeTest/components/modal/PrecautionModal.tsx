38fb6d326dddefd963cbac9af8390b0b32026c87
Soyoung Park <soyoung008@naver.com>
2024-01-09 05:48:19 +0900

    [ etc/#28 ] 상수 분리

--- nonsoolmateClient/src/takeTest/components/modal/PrecautionModal.tsx
+++ nonsoolmateClient/src/takeTest/components/modal/PrecautionModal.tsx
@@ -2,6 +2,7 @@ import styled, { css } from "styled-components";
 import Modal, { ModalContainer } from "./Modal";
 import { columnFlex, mainButtonStyle } from "style/commonStyle";
 import { PrecautionNumOneIc, PrecautionNumThreeIc, PrecautionNumTwoIc } from "@assets/index";
+import PRECAUTION_MODAL from "takeTest/constants/modalConstants";
 
 export default function PrecautionModal() {
   return (
@@ -10,39 +11,27 @@ export default function PrecautionModal() {
         <PrecautionModalBox>
           <ModalTitle>주의사항</ModalTitle>
           <ModalDetailBox>
-            <ModalDetail>
-              <PrecautionNumOneIcon />
-              <DetailContent>
-                <DetailTitle>원고지 준비</DetailTitle>
-                <DetailSubText>
-                  논술메이트의 첨삭은 이미지 파일을 통해 이루어집니다.
-                  <br />
-                  답안지를 촬영/스캔해 이미지로 제출할 수 있도록 원고지를 준비해주세요.
-                </DetailSubText>
-              </DetailContent>
-            </ModalDetail>
-            <ModalDetail>
-              <PrecautionNumTwoIcon />
-              <DetailContent>
-                <DetailTitle>제한시간 엄수</DetailTitle>
-                <DetailSubText>
-                  시험 시작 버튼 클릭과 동시에 타이머가 시작됩니다.
-                  <br />
-                  타이머가 끝나면 시험지를 더 이상 볼 수 없으니 시간 안배에 주의하세요.
-                </DetailSubText>
-              </DetailContent>
-            </ModalDetail>
-            <ModalDetail>
-              <PrecautionNumThreeIcon />
-              <DetailContent>
-                <DetailTitle>이미지 파일 제출</DetailTitle>
-                <DetailSubText>
-                  시험이 종료되면 상단 타이머가 멈추고 답안지 파일 제출 창이 나타납니다.
-                  <br />
-                  파일 제출 시간은 시험 시간에 포함되지 않으니 끝까지 시험에 집중해 주세요.
-                </DetailSubText>
-              </DetailContent>
-            </ModalDetail>
+            {PRECAUTION_MODAL.map((item) => {
+              return (
+                <ModalDetail key={item.title}>
+                  {item.index === 1 ? (
+                    <PrecautionNumOneIcon />
+                  ) : item.index === 2 ? (
+                    <PrecautionNumTwoIcon />
+                  ) : (
+                    <PrecautionNumThreeIcon />
+                  )}
+                  <DetailContent>
+                    <DetailTitle>{item.title}</DetailTitle>
+                    <DetailSubText>
+                      {item.content[0]}
+                      <br />
+                      {item.content[1]}
+                    </DetailSubText>
+                  </DetailContent>
+                </ModalDetail>
+              );
+            })}
           </ModalDetailBox>
           <TestStartButton type="button">시험 시작</TestStartButton>
         </PrecautionModalBox>
@@ -65,9 +54,10 @@ const ModalTitle = styled.h1`
   margin-bottom: 2.8rem;
 `;
 const ModalDetailBox = styled.li`
-  ${columnFlex};
-
+  display: flex;
+  flex-direction: column;
   gap: 2.8rem;
+  justify-content: center;
   padding: 3.2rem 4.3rem;
   border-radius: 12px;
   background-color: ${({ theme }) => theme.colors.grey_100};
