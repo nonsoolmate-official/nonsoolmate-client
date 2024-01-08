2b505ffe9c544a0d96cfa09fc45cb73b470b6f50
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-08 22:11:36 +0900

    [ etc/#17 ] 폴더 위치 조정

---
+++ nonsoolmateClient/src/home/homePractice/HomePractice.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export default function HomePractice() {
+  return (
+    <Container>
+      <Header>나의 학습장</Header>
+      <Content></Content>
+    </Container>
+  );
+}
+
+const Header = styled.h2`
+  display: flex;
+`;
+
+const Container = styled.section`
+  display: flex;
+  width: 93.5rem;
+  background-color: ${({ theme }) => theme.colors.black};
+`;
+
+const Content = styled.section`
+  display: flex;
+`;
---
+++ nonsoolmateClient/src/home/homeStudy/HomeStudy.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+export default function HomeStudy() {
+  return (
+    <Container>
+      <Header>나의 학습장</Header>
+      <Content></Content>
+    </Container>
+  );
+}
+
+const Header = styled.h2`
+  display: flex;
+`;
+
+const Container = styled.section`
+  display: flex;
+  width: 93.5rem;
+`;
+const Content = styled.section`
+  display: flex;
+`;
