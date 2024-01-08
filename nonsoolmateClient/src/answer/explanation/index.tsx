9455302503be4b6fc32801b6387426bf56a4d7cd
SynthiaLee <seanne0801@gmail.com>
2024-01-09 04:36:58 +0900

    [ Feat/#19 ] 첨삭 및 해제 뷰 PdfViewerWrapper 적용 및 Header props 전달

--- nonsoolmateClient/src/answer/explanation/index.tsx
+++ nonsoolmateClient/src/answer/explanation/index.tsx
@@ -1,9 +1,11 @@
 import ExplainHeader from "answer/components/ExplainHeader";
+import PdfViewerWrapper from "answer/components/PdfViewerWrapper";
 
 export default function index() {
   return (
     <>
-      <ExplainHeader />
+      <ExplainHeader testTitle="중앙대학교 - 2021 인문사회 1" />
+      <PdfViewerWrapper firstTitle="문제" secondTitle="해제" ifExplanation={true} />
     </>
   );
 }
