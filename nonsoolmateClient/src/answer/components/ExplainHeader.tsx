490fe0a3ec9695e76516c4d9d386c6704ff19c38
SynthiaLee <seanne0801@gmail.com>
2024-01-09 00:37:09 +0900

    [ Feat/#19 ] 해제 뷰 공통 헤더 생성

--- nonsoolmateClient/src/answer/components/ExplainHeader.tsx
+++ nonsoolmateClient/src/answer/components/ExplainHeader.tsx
@@ -1,3 +1,38 @@
+import { BackToListIc } from "@assets/index";
+import styled from "styled-components";
+
 export default function ExplainHeader() {
-  return <div>ExplainHeader</div>;
+  return (
+    <HeaderContainer>
+      <BackToHomeButton>
+        <BackToListIcon />
+      </BackToHomeButton>
+      <TestName>중앙대학교 - 2021 인문사회 1</TestName>
+    </HeaderContainer>
+  );
 }
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100vw;
+  height: 6.4rem;
+  padding: 1.2rem 0 1.2rem 3.6rem;
+  box-shadow: ${({ theme }) => theme.effects.top_nav_shadow};
+`;
+
+const BackToHomeButton = styled.button`
+  /* cursor: pointer; */
+`;
+
+const BackToListIcon = styled(BackToListIc)`
+  width: 4rem;
+  height: 4rem;
+`;
+
+const TestName = styled.p`
+  margin-left: 1.6rem;
+  font: ${({ theme }) => theme.fonts.Headline5};
+  color: ${({ theme }) => theme.colors.black};
+`;
