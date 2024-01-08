579fa6f0c3d0d5f5d4601bbf023d0e41b07079a6
HAE2NI <hae0_02ni@ewhain.net>
2024-01-08 21:46:26 +0900

    [ style ] header 구조 짜기

--- nonsoolmateClient/src/onbording/components/header/Header.tsx
+++ nonsoolmateClient/src/onbording/components/header/Header.tsx
@@ -1,3 +1,11 @@
+import HeaderLeft from "./HeaderLeft";
+import HeaderRight from "./HeaderRight";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <HeaderLeft />
+      <HeaderRight />
+    </>
+  );
 }
--- nonsoolmateClient/src/onbording/components/header/HeaderRight.tsx
+++ nonsoolmateClient/src/onbording/components/header/HeaderRight.tsx
@@ -1,3 +1,11 @@
+import LoginButton from "./buttons/LoginButton";
+import MembershipButton from "./buttons/MembershipButton";
+
 export default function HeaderRight() {
-  return <div>HeaderRight</div>;
+  return (
+    <>
+      <MembershipButton />
+      <LoginButton />
+    </>
+  );
 }
