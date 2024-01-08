3cf4cc790eceab2654c290cbc23f0864998b9b78
HAE2NI <hae0_02ni@ewhain.net>
2024-01-08 23:10:23 +0900

    [ design ] membership 버튼

--- nonsoolmateClient/src/onbording/components/header/buttons/MembershipButton.tsx
+++ nonsoolmateClient/src/onbording/components/header/buttons/MembershipButton.tsx
@@ -1,3 +1,13 @@
+import { commonFlex } from "style/commonStyle";
+import styled from "styled-components";
+
 export default function MembershipButton() {
-  return <div>MembershipButton</div>;
+  return <Button>멤버십</Button>;
 }
+
+const Button = styled.button`
+  ${commonFlex}
+
+  font: ${({ theme }) => theme.fonts.Body3};
+  color: ${({ theme }) => theme.colors.main_blue};
+`;
