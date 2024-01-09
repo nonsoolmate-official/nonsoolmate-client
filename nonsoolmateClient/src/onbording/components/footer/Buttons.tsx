e2345a4861fe16481a22fbf347e447163c4ccb2a
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 15:58:22 +0900

    [ design ] buttons 컴포넌트 퍼블리싱

---
+++ nonsoolmateClient/src/onbording/components/footer/Buttons.tsx
@@ -0,0 +1,27 @@
+import { commonFlex } from "style/commonStyle";
+import styled from "styled-components";
+
+export default function Buttons() {
+  return (
+    <Container>
+      <Button type="button">Product</Button>
+      <Button type="button">Information</Button>
+      <Button type="button">Company</Button>
+      <Button type="button">Terms</Button>
+      <Button type="button">Privacy</Button>
+      <Button type="button">Cookies</Button>
+    </Container>
+  );
+}
+
+const Container = styled.article`
+  ${commonFlex}
+
+  gap: 3.3rem;
+  justify-content: space-between;
+`;
+
+const Button = styled.button`
+  font: ${({ theme }) => theme.fonts.Body6};
+  color: ${({ theme }) => theme.colors.grey_600};
+`;
