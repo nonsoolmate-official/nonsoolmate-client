a653039767b9fccda126cada0dae5661383ab999
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-09 15:48:35 +0900

    [ design/#27 ] HomePractice UI

--- nonsoolmateClient/src/home/homePractice/HomePractice.tsx
+++ nonsoolmateClient/src/home/homePractice/HomePractice.tsx
@@ -1,22 +1,44 @@
 import styled from "styled-components";
+import { EmptyIc } from "@assets/index";
+import { UnsetContainerLayout } from "style/layout/UnsetContainerLayout";
+import { UnsetContentLayout } from "style/layout/UnsetContentLayout";
 
 export default function HomePractice() {
   return (
     <Container>
-      <Header>나의 학습장</Header>
-      <Content></Content>
+      <Header>나의 연습장</Header>
+      <Content>
+        <EmptyIcon />
+        <ContentText>아직 준비 중이에요!</ContentText>
+      </Content>
     </Container>
   );
 }
 
-const Header = styled.h2`
-  display: flex;
+const Container = styled.section`
+  ${UnsetContainerLayout}
 `;
 
-const Container = styled.section`
+const Header = styled.h2`
+  ${({ theme }) => theme.fonts.Headline5};
+
   display: flex;
-  width: 93.5rem;
+  margin-left: 0.8rem;
 `;
+
 const Content = styled.section`
-  display: flex;
+  ${UnsetContentLayout}
+
+  padding: 14.6rem 0;
+`;
+
+const EmptyIcon = styled(EmptyIc)`
+  width: 10.4rem;
+  height: 10.4rem;
+`;
+
+const ContentText = styled.p`
+  ${({ theme }) => theme.fonts.Body4};
+
+  color: ${({ theme }) => theme.colors.grey_700};
 `;
