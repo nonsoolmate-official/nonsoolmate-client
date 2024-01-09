0c264ffe7e34466f13b3531fc1c9c28ba4671817
SynthiaLee <seanne0801@gmail.com>
2024-01-10 02:13:31 +0900

    [ Feat/#38 ] pdf viewer 컴포넌트 기본 구현 완료

--- nonsoolmateClient/src/answer/components/PdfViewer.tsx
+++ nonsoolmateClient/src/answer/components/PdfViewer.tsx
@@ -1,11 +1,31 @@
 import styled from "styled-components";
+import { Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 export default function PdfViewer() {
-  return <PdfViewerWrapper>PdfViewer</PdfViewerWrapper>;
+  return (
+    <PdfViewerWrapper>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <div
+          style={{
+            padding: "2rem 0.8rem 0 2rem",
+            height: "calc(100vh - 16.4rem)",
+          }}>
+          <Viewer
+            fileUrl={`http://www.usrap.org/sites/default/files/historical/pdf/usRAP_brochure.pdf`}
+            theme={{ theme: "light" }}
+          />
+        </div>
+      </Worker>
+    </PdfViewerWrapper>
+  );
 }
 
 const PdfViewerWrapper = styled.div`
+  ${({ theme }) => theme.effects.pdf_shadow};
+
   width: calc((100vw - 16.8rem) / 2);
   height: calc(100vh - 16.4rem);
-  background-color: ${({ theme }) => theme.colors.main_blue};
+  border-radius: 7.679px;
 `;
