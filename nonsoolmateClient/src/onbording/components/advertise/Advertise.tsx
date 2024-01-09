a08170ad311e181b1dd2755bcfe01be758641644
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 22:07:37 +0900

    [ design ] advertise 퍼블리싱

--- nonsoolmateClient/src/onbording/components/advertise/Advertise.tsx
+++ nonsoolmateClient/src/onbording/components/advertise/Advertise.tsx
@@ -1,14 +1,23 @@
-import Advertisement from "./Advertisement/Advertisement";
+import styled from "styled-components";
 import Reasons from "./Advertisement/Reasons";
 import Summary from "./Summary";
 import Title from "./Title";
+import { columnFlex } from "style/commonStyle";
 
 export default function Advertise() {
   return (
-    <>
+    <Container>
       <Title />
       <Summary />
       <Reasons />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.section`
+  ${columnFlex}
+
+  width: 100%;
+  height: 86rem;
+  background-color: ${({ theme }) => theme.colors.grey_50};
+`;
