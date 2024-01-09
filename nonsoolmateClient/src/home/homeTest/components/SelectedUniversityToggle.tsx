6719d924ab40ba4565773facc3d9bde65a6e8e62
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-10 04:16:30 +0900

    [ design/#37 ] 토글 컴포넌트 생성

---
+++ nonsoolmateClient/src/home/homeTest/components/SelectedUniversityToggle.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function SelectedUniversityToggle() {
+  return <Container>SelectedUniversityToggle</Container>;
+}
+
+const Container = styled.section`
+  height: 32rem;
+`;
