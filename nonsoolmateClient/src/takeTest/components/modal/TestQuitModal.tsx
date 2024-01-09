8e2f8d594daf3342ee7fc19c8d9fe75155edcbe8
Soyoung Park <soyoung008@naver.com>
2024-01-09 16:03:59 +0900

    [ design/#28 ] TestQuitModal 구현

--- nonsoolmateClient/src/takeTest/components/modal/TestQuitModal.tsx
+++ nonsoolmateClient/src/takeTest/components/modal/TestQuitModal.tsx
@@ -1,3 +1,58 @@
+import styled from "styled-components";
+import Modal, { ModalContainer } from "./Modal";
+import { columnFlex, commonFlex, lightBlueButtonStyle, mainButtonStyle } from "style/commonStyle";
+
 export default function TestQuitModal() {
-  return <div>TestQuitModal</div>;
+  return (
+    <TestQuitModalContainer>
+      <Modal>
+        <TestQuitModalBox>
+          <ModalContent>
+            <ModalTitle>페이지를 나가시겠습니까?</ModalTitle>
+            <ModalText>시험 기록이 저장되지 않습니다.</ModalText>
+          </ModalContent>
+          <ButtonContainer>
+            <TestQuitButton>나가기</TestQuitButton>
+            <StayButton>머무르기</StayButton>
+          </ButtonContainer>
+        </TestQuitModalBox>
+      </Modal>
+    </TestQuitModalContainer>
+  );
 }
+const TestQuitModalContainer = styled(ModalContainer)`
+  background-color: ${({ theme }) => theme.effects.dimmed_40};
+  backdrop-filter: blur(0.3rem);
+`;
+const TestQuitModalBox = styled.div`
+  ${columnFlex};
+
+  gap: 3.8rem;
+  padding: 5.4rem 6rem 4.4rem;
+`;
+const ModalContent = styled.div`
+  ${columnFlex};
+
+  gap: 2rem;
+`;
+const ModalTitle = styled.h1`
+  ${({ theme }) => theme.fonts.Headline4};
+`;
+const ModalText = styled.p`
+  ${({ theme }) => theme.fonts.Body6};
+
+  color: ${({ theme }) => theme.colors.grey_700};
+`;
+const ButtonContainer = styled.div`
+  ${commonFlex};
+
+  gap: 1.6rem;
+`;
+const TestQuitButton = styled(lightBlueButtonStyle)`
+  width: 16rem;
+  padding: 0.8rem 0;
+`;
+const StayButton = styled(mainButtonStyle)`
+  width: 16rem;
+  padding: 0.8rem 0;
+`;
