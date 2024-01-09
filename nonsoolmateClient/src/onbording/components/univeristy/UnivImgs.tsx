dca318117f002b0d345ca0ea256e53635208f6d1
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 17:45:19 +0900

    [ design ] univimg 퍼블리싱

--- nonsoolmateClient/src/onbording/components/univeristy/UnivImgs.tsx
+++ nonsoolmateClient/src/onbording/components/univeristy/UnivImgs.tsx
@@ -1,3 +1,12 @@
+import University from "@assets/image/univieristy.png";
+import styled from "styled-components";
+
 export default function UnivImgs() {
-  return <div>UnivImg</div>;
+  return <Image src={University} alt="대학 사진들" />;
 }
+
+const Image = styled.img`
+  width: 100%;
+  height: 26.5rem;
+  margin-top: 5.6rem;
+`;
