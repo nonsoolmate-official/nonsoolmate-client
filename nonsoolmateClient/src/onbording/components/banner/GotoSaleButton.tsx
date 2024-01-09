f4185f606fea33a1e4b491e4c6918e5a6729a88a
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 17:23:08 +0900

    [ design ] gotoSaleButton 퍼블리싱

--- nonsoolmateClient/src/onbording/components/banner/GotoSaleButton.tsx
+++ nonsoolmateClient/src/onbording/components/banner/GotoSaleButton.tsx
@@ -1,3 +1,28 @@
+import { commonFlex } from "style/commonStyle";
+import styled from "styled-components";
+import { RightWhiteArrowIc } from "@assets/index";
+
 export default function GotoSaleButton() {
-  return <div>GotoSaleButton</div>;
+  return (
+    <Button>
+      첫 달 20% 할인받기 <RightWhiteArrowIcon />
+    </Button>
+  );
 }
+
+const Button = styled.button`
+  ${commonFlex}
+
+  gap: 1rem;
+  width: 25.9rem;
+  height: 4.8rem;
+  border-radius: 40px;
+  background-color: ${({ theme }) => theme.colors.black};
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.fonts.Headline5};
+`;
+
+const RightWhiteArrowIcon = styled(RightWhiteArrowIc)`
+  width: 2.4rem;
+  height: 2.4rem;
+`;
