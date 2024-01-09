e2ea83e35995d5382ca6edfa762fd88ad6c5aa26
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-09 15:47:38 +0900

    [ etc/#27 ] HomeTestUnset 뷰 확인

--- nonsoolmateClient/src/home/homeTest/HomeTest.tsx
+++ nonsoolmateClient/src/home/homeTest/HomeTest.tsx
@@ -1,12 +1,10 @@
-import styled from "styled-components";
-import HomeTestSet from "./HomeTestSet";
 import HomeTestUnset from "./HomeTestUnset";
 
 export default function HomeTest() {
   return (
     <>
-      {/* {none&&<HomeTestUnset/>}
-      {!none&&<HomeTestSet/>} */}
+      <HomeTestUnset/>
+      {/* {!none&&<HomeTestSet/>} */}
     </>
   );
 }
