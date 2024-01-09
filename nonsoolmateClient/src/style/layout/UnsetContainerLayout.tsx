e3dc6b9b69ab8d8a53609cc29b5ab06583339775
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-10 00:20:24 +0900

    [ fix/#27 ] outlet 컨테이너 width 반응형 구현

--- nonsoolmateClient/src/style/layout/UnsetContainerLayout.tsx
+++ nonsoolmateClient/src/style/layout/UnsetContainerLayout.tsx
@@ -4,7 +4,7 @@ export const UnsetContainerLayout = css`
   display: flex;
   flex-direction: column;
   gap: 0.7rem;
-  width: 93.5rem;
+  width: calc(100% - 43.1rem);
   height: 100%;
   padding: 6.7rem 21.5rem 0 2.4rem;
   background-color: ${({ theme }) => theme.colors.grey_50};
