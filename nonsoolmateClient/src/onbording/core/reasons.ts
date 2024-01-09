e6e6ac88f1803a03861d47f039cc5cf88cad2621
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 22:08:39 +0900

    [ etc ] core 부분 수정

--- nonsoolmateClient/src/onbording/core/reasons.ts
+++ nonsoolmateClient/src/onbording/core/reasons.ts
@@ -10,32 +10,38 @@ import { ReasonListType } from "onbording/types/reasonListType";
 export const REASON_LIST: ReasonListType[] = [
   {
     title: "학원보다 많은 데이터",
-    summary: "전국의 논술학습 데이터를 모두 모아 논술메이트에서 한번에!",
+    summary: "전국의 논술학습 데이터를 모두 모아",
+    summary2: "논술메이트에서 한번에!",
     img: ReasonIc01,
   },
   {
     title: "나에게 맞춤형 첨삭",
-    summary: "논술메이트의 논술연구소가 제공하는 개인화된 첨삭 및 코칭",
+    summary: "논술메이트의 논술연구소가 제공하는",
+    summary2: "개인화된 첨삭 및 코칭",
     img: ReasonIc02,
   },
   {
     title: "학원보다 75% 낮은 가격",
-    summary: "한달에 50만원이 넘는 학원비가 아깝다면? 10만원 대의 논술메이트가 정답!",
+    summary: "한달에 50만원이 넘는 학원비가 아깝다면?",
+    summary2: "10만원 대의 논술메이트가 정답!",
     img: ReasonIc03,
   },
   {
     title: "시공간적 제약 X",
-    summary: "이동시간에도 논술 공부를 하고 싶다면? 논술메이트에서 간편하게!",
+    summary: "이동시간에도 논술 공부를 하고 싶다면?",
+    summary2: "논술메이트에서 간편하게!",
     img: ReasonIc04,
   },
   {
     title: "체계적인 커리큘럼",
-    summary: "학교별 출제/체점 성향 분석부터 기출 해제, 풀이 채점까지",
+    summary: "학교별 출제/체점 성향 분석부터 기출 해제,",
+    summary2: "풀이 채점까지",
     img: ReasonIc05,
   },
   {
     title: "꾸준한 학습",
-    summary: "장기적으로 제공되는 학습자료, 개인별 진도체크와 개인화된 학습 분석·관리 시스템",
+    summary: "장기적으로 제공되는 학습자료, 개인별",
+    summary2: "진도체크와 개인화된 학습 분석·관리 시스템",
     img: ReasonIc06,
   },
 ];
