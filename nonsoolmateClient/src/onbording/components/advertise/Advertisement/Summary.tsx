c5474a27db7012bdefee9898478b67d5fa68674c
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 22:09:08 +0900

    [ design ] summary 부분 퍼블리싱

--- nonsoolmateClient/src/onbording/components/advertise/Advertisement/Summary.tsx
+++ nonsoolmateClient/src/onbording/components/advertise/Advertisement/Summary.tsx
@@ -1,3 +1,29 @@
-export default function Summary() {
-  return <div>Summary</div>;
+import { columnFlex } from "style/commonStyle";
+import styled from "styled-components";
+
+interface SummaryProp {
+  summaryText: string;
+  summaryText2: string;
 }
+
+export default function Summary(props: SummaryProp) {
+  const { summaryText, summaryText2 } = props;
+  return (
+    <Container>
+      <Text>{summaryText}</Text>
+      <Text>{summaryText2}</Text>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  ${columnFlex}
+
+  justify-content: flex-start;
+`;
+
+const Text = styled.p`
+  width: 25.9rem;
+  color: ${({ theme }) => theme.colors.grey_700};
+  ${({ theme }) => theme.fonts.Body6};
+`;
