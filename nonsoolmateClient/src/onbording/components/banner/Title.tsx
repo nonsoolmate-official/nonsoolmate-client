cd4822ddf41264c9c06e86e76c0ba42e3585adc3
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 16:37:34 +0900

    [ design ] banner title 퍼블리싱

--- nonsoolmateClient/src/onbording/components/banner/Title.tsx
+++ nonsoolmateClient/src/onbording/components/banner/Title.tsx
@@ -1,3 +1,19 @@
+import { columnFlex, commonFlex } from "style/commonStyle";
+import styled from "styled-components";
+
 export default function Title() {
-  return <div>Title</div>;
+  return (
+    <Container>
+      <Text>논술메이트,</Text> <Text> 논술입시 성공의 지름길</Text>
+    </Container>
+  );
 }
+
+const Container = styled.article`
+  ${columnFlex}
+`;
+
+const Text = styled.h1`
+  font: ${({ theme }) => theme.fonts.Headline1};
+  color: ${({ theme }) => theme.colors.white};
+`;
