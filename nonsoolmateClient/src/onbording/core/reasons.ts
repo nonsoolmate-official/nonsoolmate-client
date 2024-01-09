b8c42df02cd08e3cb76cf0d90c4fb003f61bb6b9
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 18:49:24 +0900

    [ etc ] reason lIST 추가

---
+++ nonsoolmateClient/src/onbording/core/reasons.ts
@@ -0,0 +1,41 @@
+import ReasonIc01 from "@assets/image/reason01Ic.png";
+import ReasonIc02 from "@assets/image/reason02Ic.png";
+import ReasonIc03 from "@assets/image/reason03Ic.png";
+import ReasonIc04 from "@assets/image/reason04Ic.png";
+import ReasonIc05 from "@assets/image/reason05Ic.png";
+import ReasonIc06 from "@assets/image/reason06Ic.png";
+
+import { ReasonListType } from "onbording/types/reasonListType";
+
+export const REASON_LIST: ReasonListType[] = [
+  {
+    title: "학원보다 많은 데이터",
+    summary: "전국의 논술학습 데이터를 모두 모아 논술메이트에서 한번에!",
+    img: ReasonIc01,
+  },
+  {
+    title: "나에게 맞춤형 첨삭",
+    summary: "논술메이트의 논술연구소가 제공하는 개인화된 첨삭 및 코칭",
+    img: ReasonIc02,
+  },
+  {
+    title: "학원보다 75% 낮은 가격",
+    summary: "한달에 50만원이 넘는 학원비가 아깝다면? 10만원 대의 논술메이트가 정답!",
+    img: ReasonIc03,
+  },
+  {
+    title: "시공간적 제약 X",
+    summary: "이동시간에도 논술 공부를 하고 싶다면? 논술메이트에서 간편하게!",
+    img: ReasonIc04,
+  },
+  {
+    title: "체계적인 커리큘럼",
+    summary: "학교별 출제/체점 성향 분석부터 기출 해제, 풀이 채점까지",
+    img: ReasonIc05,
+  },
+  {
+    title: "꾸준한 학습",
+    summary: "장기적으로 제공되는 학습자료, 개인별 진도체크와 개인화된 학습 분석·관리 시스템",
+    img: ReasonIc06,
+  },
+];
