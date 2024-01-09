c3d4d19a72143de118524409bd385af15eccbe23
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-09 15:50:25 +0900

    [ design/#27 ] UnsetContentLayout padding값 삭제

--- nonsoolmateClient/src/style/layout/UnsetContentLayout.tsx
+++ nonsoolmateClient/src/style/layout/UnsetContentLayout.tsx
@@ -6,7 +6,6 @@ export const UnsetContentLayout = css`
   gap: 0.8rem;
   align-items: center;
   height: 48.7rem;
-  padding: 14.6rem 0;
   border-radius: 8px;
   background-color: ${({ theme }) => theme.colors.grey_200};
 `;
