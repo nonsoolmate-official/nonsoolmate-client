3e0a9d916fbc675477355370057a40c657dc034b
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 15:58:52 +0900

    [ design ] footer right 퍼블리싱

--- nonsoolmateClient/src/onbording/components/footer/FooterRight.tsx
+++ nonsoolmateClient/src/onbording/components/footer/FooterRight.tsx
@@ -1,3 +1,19 @@
+import styled from "styled-components";
+import Buttons from "./Buttons";
+import Sns from "./Sns";
+import { commonFlex } from "style/commonStyle";
+
 export default function FooterRight() {
-  return <div>FooterRight</div>;
+  return (
+    <Container>
+      <Buttons />
+      <Sns />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  ${commonFlex}
+
+  gap: 2.2rem;
+`;
