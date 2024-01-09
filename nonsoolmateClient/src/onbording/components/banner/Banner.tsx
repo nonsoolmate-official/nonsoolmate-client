ffe902533f9e586f132ae1cfc38d20abc87c3722
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 16:37:46 +0900

    [ design ] banner background 퍼블리싱

--- nonsoolmateClient/src/onbording/components/banner/Banner.tsx
+++ nonsoolmateClient/src/onbording/components/banner/Banner.tsx
@@ -1,15 +1,25 @@
+import styled from "styled-components";
 import GotoSaleButton from "./GotoSaleButton";
 import HomeImg from "./HomeImg";
 import Summary from "./Summary";
 import Title from "./Title";
+import { columnFlex } from "style/commonStyle";
 
 export default function Banner() {
   return (
-    <>
+    <Container>
       <Title />
       <Summary />
       <GotoSaleButton />
       <HomeImg />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.section`
+  ${columnFlex};
+
+  width: 100%;
+  height: 76.8rem;
+  background: linear-gradient(to bottom, rgb(100 120 255), rgb(255 255 255));
+`;
