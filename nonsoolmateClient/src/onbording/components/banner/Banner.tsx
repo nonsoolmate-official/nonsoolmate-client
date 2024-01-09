c18918aec9f13429a65e5e8eacc13626d3a4969a
HAE2NI <hae0_02ni@ewhain.net>
2024-01-09 17:23:53 +0900

    [ design ] banner 부분 퍼블리싱

--- nonsoolmateClient/src/onbording/components/banner/Banner.tsx
+++ nonsoolmateClient/src/onbording/components/banner/Banner.tsx
@@ -7,19 +7,37 @@ import { columnFlex } from "style/commonStyle";
 
 export default function Banner() {
   return (
-    <Container>
-      <Title />
-      <Summary />
-      <GotoSaleButton />
-      <HomeImg />
-    </Container>
+    <Wrapper>
+      <Container>
+        <TextsContainer>
+          <Title />
+          <Summary />
+          <GotoSaleButton />
+        </TextsContainer>
+        <HomeImg />
+      </Container>
+    </Wrapper>
   );
 }
 
-const Container = styled.section`
+const Wrapper = styled.section`
   ${columnFlex};
 
+  justify-content: flex-end;
   width: 100%;
   height: 76.8rem;
-  background: linear-gradient(to bottom, rgb(100 120 255), rgb(255 255 255));
+  background: linear-gradient(to bottom, rgba(101 121 255) 20%, rgb(156 169 255) 100%);
+`;
+
+const Container = styled.div`
+  ${columnFlex};
+
+  gap: 3.2rem;
+  justify-content: space-between;
+`;
+
+const TextsContainer = styled.div`
+  ${columnFlex};
+
+  gap: 2.8rem;
 `;
