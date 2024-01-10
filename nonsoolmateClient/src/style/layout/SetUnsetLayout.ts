e607166723c9b3f66f5dc39008e06f0d33259001
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-10 13:54:12 +0900

    [ design/#37 ] outlet에 overflow:scroll속성 추가하여 스크롤 생기게 하기

--- nonsoolmateClient/src/style/layout/SetUnsetLayout.ts
+++ nonsoolmateClient/src/style/layout/SetUnsetLayout.ts
@@ -5,6 +5,7 @@ export const SetUnsetContainerLayout = css`
   display: flex;
   flex-direction: column;
   gap: 0.7rem;
+  overflow: scroll;
   width: calc(100% - 43.1rem);
   height: 100%;
   padding: 6.7rem 21.5rem 0 2.4rem;
