4166b62885b7cbb9dfd0b83015fad2d6ec37bd9b
HAE2NI <hae0_02ni@ewhain.net>
2024-01-10 17:48:00 +0900

    [ design ] divideline 퍼블리싱

--- nonsoolmateClient/src/onbording/components/faq/DivideLine.tsx
+++ nonsoolmateClient/src/onbording/components/faq/DivideLine.tsx
@@ -1,3 +1,11 @@
+import styled from "styled-components";
+
 export default function DivideLine() {
-  return <div>DivideLine</div>;
+  return <Line />;
 }
+
+const Line = styled.div`
+  display: flex;
+  width: 93.6rem;
+  border-top: 1px solid ${({ theme }) => theme.colors.grey_200};
+`;
