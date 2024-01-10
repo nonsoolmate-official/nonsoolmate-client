8539453d6cb12abed921257396c96bd8fbd94118
HAE2NI <hae0_02ni@ewhain.net>
2024-01-10 18:16:58 +0900

    [ design ] Qna 퍼블리싱

--- nonsoolmateClient/src/onbording/components/faq/qna/QnA.tsx
+++ nonsoolmateClient/src/onbording/components/faq/qna/QnA.tsx
@@ -3,30 +3,35 @@ import DivideLine from "../DivideLine";
 import { QnaTypes } from "../types/qaatypes";
 import Answer from "./Answer";
 import Question from "./Question";
+import styled from "styled-components";
+import { columnFlex, commonFlex } from "style/commonStyle";
 
-interface QnAprops extends QnaTypes {
-  handleToggleOpen: () => void;
-  isToggleOpen: boolean;
-}
+interface QnAprops extends QnaTypes {}
 
 export default function QnA(props: QnAprops) {
   const { question, answer, answer2 } = props;
   const [isToggleOpen, setIsToggleOpen] = useState<boolean>(false);
 
   function handleToggleOpen() {
-    setIsToggleOpen(true);
+    setIsToggleOpen((prev) => !prev);
   }
 
   return (
     <>
-      <Question question={question} onClick={handleToggleOpen} />
+      <Question isToggleOpen={isToggleOpen} question={question} onClick={handleToggleOpen} />
       {isToggleOpen && (
-        <>
+        <AnswerContainer>
           <Answer answer={answer} />
           {answer2 && <Answer answer={answer2} />}
-        </>
+        </AnswerContainer>
       )}
       <DivideLine />
     </>
   );
 }
+
+const AnswerContainer = styled.p`
+  ${columnFlex}
+
+  margin-bottom: 2rem;
+`;
