e0eb270c807159d4a61c24ca6fbb575d05201ce5
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-10 15:08:49 +0900

    [ design/#37 ] 토글 border값에 solid속성 주기

--- nonsoolmateClient/src/home/homeTest/components/SelectedUniversityToggle.tsx
+++ nonsoolmateClient/src/home/homeTest/components/SelectedUniversityToggle.tsx
@@ -43,9 +43,12 @@ const ToggleContainer = styled.section`
   display: inline-flex;
   flex-direction: column;
   align-items: flex-start;
-  width: 69.6rem;
+  overflow: scroll;
+  width: 100%;
+  max-height: 33.2rem;
   padding: 0.8rem 2.4rem 1.6rem;
   border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.white};
 `;
 
 const ExamContainer = styled.section`
@@ -54,6 +57,8 @@ const ExamContainer = styled.section`
   justify-content: space-between;
   width: 100%;
   padding: 0.8rem 0 1.2rem;
+  border-bottom: 1px solid;
+  border-bottom-color: ${({ theme }) => theme.colors.grey_100};
 `;
 
 const ExamBox = styled.section`
@@ -72,15 +77,17 @@ const TimeLimit = styled.h3`
   color: ${({ theme }) => theme.colors.grey_500};
 `;
 
-const ButtonBox = styled.button`
+const ButtonBox = styled.section`
   display: flex;
-  gap: 0.4rem;
+  gap: 0.8rem 0;
+  align-items: center;
+  padding: 0.8rem 0;
+  border-radius: 4px;
 `;
 
 const TakeTestButton = styled(lightBlueButtonStyle)`
   width: 10rem;
-
+  height: 100%;
+  padding: 0;
   ${({ theme }) => theme.fonts.Body5};
-
-  padding: 0.4rem 0.8rem;
 `;
