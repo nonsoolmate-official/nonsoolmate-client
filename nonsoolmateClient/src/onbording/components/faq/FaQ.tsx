6af40aa2d1e57253a43b4d629c0f3ec4235f400c
HAE2NI <hae0_02ni@ewhain.net>
2024-01-10 17:48:13 +0900

    [ design ] faq 퍼블리싱

--- nonsoolmateClient/src/onbording/components/faq/FaQ.tsx
+++ nonsoolmateClient/src/onbording/components/faq/FaQ.tsx
@@ -1,13 +1,40 @@
+import styled from "styled-components";
+import { QNA_LIST } from "./core/qnalist";
 import DivideLine from "./DivideLine";
-import QnA from "./QnA";
+import QnA from "./qna/QnA";
 import Title from "./Title";
+import { columnFlex, commonFlex } from "style/commonStyle";
 
 export default function FaQ() {
   return (
-    <>
-      <Title />
-      <DivideLine />
-      <QnA />
-    </>
+    <Container>
+      <ContentContainer>
+        <Title />
+        <DivideLine />
+        {QNA_LIST.map((qna) => {
+          const { question, answer, answer2 } = qna;
+          return (
+            <>
+              <QnA question={question} answer={answer} answer2={answer2} />
+            </>
+          );
+        })}
+      </ContentContainer>
+    </Container>
   );
 }
+
+const Container = styled.section`
+  ${commonFlex}
+
+  width: 100%;
+  height: 86rem;
+`;
+
+const ContentContainer = styled.section`
+  ${columnFlex}
+
+  align-items: flex-start;
+  width: 93.6rem;
+  height: 44rem;
+`;
