3f54915d9ddf3e5ed57dbe6b110cb3e3557eb318
SynthiaLee <seanne0801@gmail.com>
2024-01-11 02:47:23 +0900

    [ Refact/#52 ] 공통 컴포넌트 재사용, 불필요 컴포넌트 및 코드 삭제

--- nonsoolmateClient/src/answer/explanation/index.tsx
+++ nonsoolmateClient/src/answer/explanation/index.tsx
@@ -1,23 +1,16 @@
 import ExplainHeader from "answer/components/ExplainHeader";
 import PdfViewerWrapper from "answer/components/PdfViewerWrapper";
-import ImagePdfWrapper from "./components/ImagePdfWrapper";
 
 export default function index() {
   return (
     <>
       <ExplainHeader testTitle="중앙대학교 - 2021 인문사회 1" />
-      <ImagePdfWrapper
+      <PdfViewerWrapper
         firstTitle="문제"
         secondTitle="해제"
         ifExplanation={true}
         pdfUrl="http://www.usrap.org/sites/default/files/historical/pdf/usRAP_brochure.pdf"
       />
-      {/* <PdfViewerWrapper
-        firstTitle="문제"
-        secondTitle="해제"
-        ifExplanation={true}
-        pdfUrl="http://www.usrap.org/sites/default/files/historical/pdf/usRAP_brochure.pdf"
-      /> */}
     </>
   );
 }
