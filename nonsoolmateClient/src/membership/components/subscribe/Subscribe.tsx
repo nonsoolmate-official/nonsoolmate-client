d1778c5ea8b2f791a8dfbf51fdd8f592f290b136
HAE2NI <hae0_02ni@ewhain.net>
2024-01-11 18:36:05 +0900

    [ design ] subscribe 퍼블리싱

--- nonsoolmateClient/src/membership/components/subscribe/Subscribe.tsx
+++ nonsoolmateClient/src/membership/components/subscribe/Subscribe.tsx
@@ -1,23 +1,33 @@
 import Title from "./Title";
 import Icons from "./Icons";
-import Price from "./Price";
 import PurchaseButton from "./PurchaseButton";
 import Sales from "./sales/Sales";
 import Summary from "./Summary";
 import { ContentListType } from "membership/types/contentlisttpye";
+import styled from "styled-components";
+import { columnFlex } from "style/commonStyle";
 
 interface ContentsProps extends ContentListType {}
 
 export default function Subscribe(props: ContentsProps) {
   const { id, title, summary, sales, price } = props;
   return (
-    <>
+    <Container>
       <Icons id={id} />
       <Title title={title} />
       <Summary summary={summary} />
-      <Sales sales={sales} />
-      <Price price={price} />
+      <Sales sales={sales} price={price} />
       <PurchaseButton />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.article`
+  ${columnFlex}
+  ${({ theme }) => theme.effects.membership_shadow};
+
+  width: 29.6rem;
+  height: 36.4rem;
+
+  border-radius: 12px;
+`;
