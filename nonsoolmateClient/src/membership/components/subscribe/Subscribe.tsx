3674c7c5b423d0e6a9feba220cb05d88fbba5e72
HAE2NI <hae0_02ni@ewhain.net>
2024-01-11 17:35:28 +0900

    [ design ] subscribe 퍼블리싱

---
+++ nonsoolmateClient/src/membership/components/subscribe/Subscribe.tsx
@@ -0,0 +1,23 @@
+import Title from "./Title";
+import Icons from "./Icons";
+import Price from "./Price";
+import PurchaseButton from "./PurchaseButton";
+import Sales from "./Sales";
+import Summary from "./Summary";
+import { ContentListType } from "membership/types/contentlisttpye";
+
+interface ContentsProps extends ContentListType {}
+
+export default function Subscribe(props: ContentsProps) {
+  const { id, title, summary, sales, price } = props;
+  return (
+    <>
+      <Icons id={id} />
+      <Title title={title} />
+      <Summary summary={summary} />
+      <Sales sales={sales} />
+      <Price price={price} />
+      <PurchaseButton />
+    </>
+  );
+}
