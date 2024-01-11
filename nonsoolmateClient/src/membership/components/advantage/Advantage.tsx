6e722f926670a23629557cea04f3a94d478051c9
HAE2NI <hae0_02ni@ewhain.net>
2024-01-11 21:45:58 +0900

    [ design ] advertise 퍼블리싱

---
+++ nonsoolmateClient/src/membership/components/advantage/Advantage.tsx
@@ -0,0 +1,16 @@
+import { columnFlex } from "style/commonStyle";
+import styled from "styled-components";
+import Summary from "./Summary";
+
+export default function Advantage() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  );
+}
+
+const Container = styled.article`
+  ${columnFlex}
+  align-items: flex-start;
+`;
