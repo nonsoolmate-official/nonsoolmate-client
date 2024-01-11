84f133dd10fb1dea52192bdd343b4f042a95a7f3
HAE2NI <hae0_02ni@ewhain.net>
2024-01-11 18:35:57 +0900

    [ design ] purchase 퍼블리싱

--- nonsoolmateClient/src/membership/components/subscribe/PurchaseButton.tsx
+++ nonsoolmateClient/src/membership/components/subscribe/PurchaseButton.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { commonFlex } from "style/commonStyle";
+import styled from "styled-components";
 
 export default function PurchaseButton() {
-  return <div>PurchaseButton</div>;
+  return <Button>구매하기</Button>;
 }
+
+const Button = styled.button`
+  ${commonFlex}
+  ${({ theme }) => theme.fonts.Body1};
+
+  border-radius: 8px;
+  width: 25.6rem;
+  height: 4.8rem;
+
+  background-color: ${({ theme }) => theme.colors.main_blue};
+  color: ${({ theme }) => theme.colors.white};
+`;
