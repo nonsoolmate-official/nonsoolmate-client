e03cff610fe726c0a62dddc650fb036279cf6d56
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-11 13:32:56 +0900

    [ feat/#51 ] 모달 띄우기위해 TestSet페이지와 TestUnset페이지에 props전달

--- nonsoolmateClient/src/home/homeTest/HomeTest.tsx
+++ nonsoolmateClient/src/home/homeTest/HomeTest.tsx
@@ -1,11 +1,33 @@
 import HomeTestUnset from "./HomeTestUnset";
 import HomeTestSet from "./HomeTestSet";
+import UniversityModal from "home/components/modal/UniversityModal";
+import { useState } from "react";
 
 export default function HomeTest() {
+  const [universityModal, setUniversityModal] = useState<boolean>(false);
+  const [selectedUniversityIds, setSelectedUniversityIds] = useState<number[]>([]);
+
+  function handleUniversityModal(open: boolean) {
+    setUniversityModal(open);
+  }
+
+  function handleSelectedUniversityIds(IDs: number[]) {
+    setSelectedUniversityIds(IDs);
+  }
   return (
     <>
-      {/* {none&&<HomeTestUnSet/>} */}
-      <HomeTestSet />
+      {selectedUniversityIds.length > 0 ? (
+        <HomeTestSet selectedUniversityIds={selectedUniversityIds} handleUniversityModal={handleUniversityModal} />
+      ) : (
+        <HomeTestUnset handleUniversityModal={handleUniversityModal}/>
+      )}
+      {universityModal && (
+        <UniversityModal
+          selectedUniversityIds={selectedUniversityIds}
+          handleUniversityModal={handleUniversityModal}
+          handleSelectedUniversityIds={handleSelectedUniversityIds}
+        />
+      )}
     </>
   );
 }
