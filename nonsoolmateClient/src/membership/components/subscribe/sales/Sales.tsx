a316f250135d9972a24c094afef39efa21b0560f
HAE2NI <hae0_02ni@ewhain.net>
2024-01-11 18:36:30 +0900

    [ design ] sales 퍼블리싱

--- nonsoolmateClient/src/membership/components/subscribe/sales/Sales.tsx
+++ nonsoolmateClient/src/membership/components/subscribe/sales/Sales.tsx
@@ -1,21 +1,32 @@
 import { SalesType } from "membership/types/salestype";
 import SalesContents from "./SalesContents";
+import styled from "styled-components";
+import { columnFlex } from "style/commonStyle";
+import Price from "../Price";
 
 interface SalesProp {
   sales: SalesType[];
+  price: number;
 }
 
 export default function Sales(props: SalesProp) {
-  const { sales } = props;
+  const { sales, price } = props;
 
   return (
-    <>
+    <Container>
       {sales.map((sale) => {
         const { saletitle, beforeprice, salepercent } = sale;
         return (
           <SalesContents key={saletitle} saletitle={saletitle} beforeprice={beforeprice} salepercent={salepercent} />
         );
       })}
-    </>
+      <Price price={price} />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  ${columnFlex}
+  gap: 0.3rem;
+  align-items: flex-end;
+`;
