ee97a382bd5a59105c8ac285b2dee92cc13ed898
HAE2NI <hae0_02ni@ewhain.net>
2024-01-11 16:02:42 +0900

    [ feat ] header page에 맞춰서 styled components props 다르게

--- nonsoolmateClient/src/components/onbordingheader/Header.tsx
+++ nonsoolmateClient/src/components/onbordingheader/Header.tsx
@@ -3,19 +3,28 @@ import HeaderLeft from "./HeaderLeft";
 import HeaderRight from "./HeaderRight";
 import styled from "styled-components";
 
-export default function Header() {
+interface HeaderProps {
+  isOnbording: boolean;
+}
+
+export default function Header(props: HeaderProps) {
+  const { isOnbording } = props;
+
   return (
-    <Container>
+    <Container $isOnbording={isOnbording}>
       <HeaderLeft />
       <HeaderRight />
     </Container>
   );
 }
 
-const Container = styled.header`
+const Container = styled.header<{ $isOnbording: boolean }>`
   ${commonFlex}
 
   justify-content: space-around;
   width: 100%;
   height: 6.4rem;
+  box-shadow: 
+  ${($isOnbording) => ($isOnbording ?  0 0 12px 0 rgb(0 0 0 / 10%) : null)}
+ 
 `;
