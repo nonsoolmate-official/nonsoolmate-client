3278e3ea4c3e86118f06acfd756a8714867746f5
HAE2NI <hae0_02ni@ewhain.net>
2024-01-11 15:38:32 +0900

    [ style ] import 수정

--- nonsoolmateClient/src/onbording/index.tsx
+++ nonsoolmateClient/src/onbording/index.tsx
@@ -2,7 +2,7 @@ import Advertise from "./components/advertise/Advertise";
 import Banner from "./components/banner/Banner";
 import FaQ from "./components/faq/FaQ";
 import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
+import Header from "../components/onbordingheader/Header";
 import Univeristy from "./components/univeristy/Univeristy";
 
 export default function OnBording() {
