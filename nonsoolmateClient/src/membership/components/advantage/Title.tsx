5e1fe566d37daba4540e5b9b2cefa05989e6fa29
HAE2NI <hae0_02ni@ewhain.net>
2024-01-11 21:46:22 +0900

    [ design ] title 퍼블리싱

---
+++ nonsoolmateClient/src/membership/components/advantage/Title.tsx
@@ -0,0 +1,18 @@
+import { commonFlex } from "style/commonStyle";
+import styled from "styled-components";
+
+export default function AdvanTitle() {
+  return <Text>멤버쉽 혜택</Text>;
+}
+
+const Text = styled.h1`
+  ${commonFlex}
+  ${({ theme }) => theme.fonts.Headline3};
+
+  width: 21.7rem;
+  height: 5.2rem;
+
+  border-radius: 4px;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.grey_900};
+`;
