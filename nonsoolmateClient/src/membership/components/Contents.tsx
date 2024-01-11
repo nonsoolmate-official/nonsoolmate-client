0902dc5602fc95412ff7b8413699bc62f60bf190
HAE2NI <hae0_02ni@ewhain.net>
2024-01-11 21:42:38 +0900

    [ design ] contents 퍼블리싱

---
+++ nonsoolmateClient/src/membership/components/Contents.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import Subscribe from "./subscribe/Subscribe";
+import { CONTENT_LIST } from "../core/contentlist";
+import { columnFlex, commonFlex } from "style/commonStyle";
+import Advantage from "./advantage/Advantage";
+import BackgroundGrey from "./BackgroundGrey";
+import AdvanTitle from "./advantage/Title";
+
+export default function Contents() {
+  return (
+    <Container>
+      <BackgroundGrey />
+      {CONTENT_LIST.map((ele) => {
+        const { id, title, summary, sales, price } = ele;
+        return <Subscribe key={id} id={id} title={title} summary={summary} sales={sales} price={price} />;
+      })}
+      <AdvantageContainer>
+        <AdvanTitle />
+        <Advantage />
+      </AdvantageContainer>
+    </Container>
+  );
+}
+
+const Container = styled.section`
+  ${commonFlex}
+  gap: 2.4rem;
+  margin-top: 8.1rem;
+`;
+
+const AdvantageContainer = styled.article`
+  ${columnFlex}
+
+  justify-content: space-between;
+  align-items: flex-start;
+  height: 34.5rem;
+`;
