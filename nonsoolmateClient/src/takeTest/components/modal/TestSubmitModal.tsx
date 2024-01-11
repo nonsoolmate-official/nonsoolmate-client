9a113c34aef4b836ab414af28e5e87919a126430
Soyoung Park <soyoung008@naver.com>
2024-01-11 22:40:59 +0900

    [ feat/#60 ] 파일 압축 구현

--- nonsoolmateClient/src/takeTest/components/modal/TestSubmitModal.tsx
+++ nonsoolmateClient/src/takeTest/components/modal/TestSubmitModal.tsx
@@ -1,13 +1,49 @@
 import styled from "styled-components";
 import Modal, { ModalContainer } from "./Modal";
 import { columnFlex, mainButtonStyle } from "style/commonStyle";
+import JSZip from "jszip";
 
 interface TestSubmitProps {
   isFile: File[] | null;
 }
 export default function TestSubmitModal(props: TestSubmitProps) {
   const { isFile } = props;
+  let zip = new JSZip();
 
+  const handleZipCreation = () => {
+    if (isFile) {
+      let filePromises = isFile.map((file) => {
+        return new Promise<void>((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onload = function (event) {
+            const target = event.target;
+            if (target && target.result) {
+              zip.file(file.name, target.result, { binary: true });
+              resolve();
+            } else {
+              reject("File reading failed");
+            }
+          };
+          reader.onerror = reject;
+          reader.readAsBinaryString(file);
+        });
+      });
+
+      Promise.all(filePromises)
+        .then(() => {
+          return zip.generateAsync({ type: "blob" });
+        })
+        .then((blob) => {
+          //saveAs(blob,"files.zip")
+          //mutate(blob)
+          console.log(blob);
+          console.log("Blob type:", blob.type);
+        })
+        .catch((error) => {
+          console.error("An error occurred:", error);
+        });
+    }
+  };
   return (
     <TestSubmitModalCotainer>
       <Modal>
@@ -15,7 +51,7 @@ export default function TestSubmitModal(props: TestSubmitProps) {
           <ModalContent>
             <ModalTitle>아래 파일을 제출하시겠습니까?</ModalTitle>
             <ModalFile>{isFile?.map((item) => <FileName key={item.name}>{item.name}</FileName>)}</ModalFile>
-            <SubmitButton>제출하기</SubmitButton>
+            <SubmitButton onClick={handleZipCreation}>제출하기</SubmitButton>
           </ModalContent>
         </TestSubmitModalBox>
       </Modal>
