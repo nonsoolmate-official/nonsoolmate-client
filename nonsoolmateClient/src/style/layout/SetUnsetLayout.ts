9e55b9ae281311f260deaf6daeca3a487456e6ee
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-13 02:24:18 +0900

    [ refactor/#73 ] setUnset페이지 공통 Layout 수정

--- nonsoolmateClient/src/style/layout/SetUnsetLayout.ts
+++ nonsoolmateClient/src/style/layout/SetUnsetLayout.ts
@@ -1,4 +1,3 @@
-import { columnFlex } from "style/commonStyle";
 import { css } from "styled-components";
 
 export const SetUnsetContainerLayout = css`
@@ -7,16 +6,16 @@ export const SetUnsetContainerLayout = css`
   overflow: scroll;
   width: calc(100% - 43.1rem);
   height: 100%;
-  padding: 6.7rem 21.5rem 0 2.4rem;
+  padding: 6.7rem 21.5rem 11.5rem 2.4rem;
   background-color: ${({ theme }) => theme.colors.grey_50};
 `;
 
 export const UnsetContentLayout = css`
-  ${columnFlex}
-
+  display: flex;
+  flex-direction: column;
   gap: 0.8rem;
   align-items: center;
-  height: calc(100% - 18.2rem);
+  height: 100%;
   border-radius: 8px;
   background-color: ${({ theme }) => theme.colors.grey_200};
 `;
