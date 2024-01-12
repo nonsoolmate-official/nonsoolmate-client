4f3d2441f1064c22c8693e2fbbb7816e62d7684c
HAE2NI <hae0_02ni@ewhain.net>
2024-01-12 20:42:48 +0900

    [ design ] navigate 추가 (로그인 버튼)

--- nonsoolmateClient/src/components/onbordingheader/buttons/LoginButton.tsx
+++ nonsoolmateClient/src/components/onbordingheader/buttons/LoginButton.tsx
@@ -1,8 +1,18 @@
 import { commonFlex } from "style/commonStyle";
 import styled from "styled-components";
+import { useNavigate } from "react-router";
 
 export default function LoginButton() {
-  return <Button type="button">로그인</Button>;
+  const navigate = useNavigate();
+  return (
+    <Button
+      type="button"
+      onClick={() => {
+        navigate("/signup");
+      }}>
+      로그인
+    </Button>
+  );
 }
 
 const Button = styled.button`
