755440f6d944efcc6212adfb961f26d1e3587d01
HAE2NI <hae0_02ni@ewhain.net>
2024-01-12 20:42:58 +0900

    [ design ] membershipbutton navigate 추가

--- nonsoolmateClient/src/components/onbordingheader/buttons/MembershipButton.tsx
+++ nonsoolmateClient/src/components/onbordingheader/buttons/MembershipButton.tsx
@@ -1,8 +1,18 @@
 import { commonFlex } from "style/commonStyle";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 export default function MembershipButton() {
-  return <Button>멤버십</Button>;
+  const navigate = useNavigate();
+
+  return (
+    <Button
+      onClick={() => {
+        navigate("/membership");
+      }}>
+      멤버십
+    </Button>
+  );
 }
 
 const Button = styled.button`
