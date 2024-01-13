fba9e3f2a3fb390d25d659f3b94fb9eea295b188
SynthiaLee <seanne0801@gmail.com>
2024-01-13 22:44:11 +0900

    [ Init/#88 ] axios instance 생성

---
+++ nonsoolmateClient/src/api/axios.ts
@@ -0,0 +1,6 @@
+import axios, { AxiosInstance } from "axios";
+
+export const client: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+});
