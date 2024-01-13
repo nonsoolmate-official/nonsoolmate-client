79e63bc641657a855cfd05d2c9ae8535ee2493a1
SynthiaLee <seanne0801@gmail.com>
2024-01-13 23:36:59 +0900

    [ Init/#88 ] 코드리뷰 사항 반영

--- nonsoolmateClient/src/api/axios.ts
+++ nonsoolmateClient/src/api/axios.ts
@@ -2,5 +2,10 @@ import axios, { AxiosInstance } from "axios";
 
 export const client: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsIm1lbWJlcklkIjoyLCJlbWFpbCI6InRoZGFsc3JiNzlAbmF2ZXIuY29tIiwiaWF0IjoxNzA1MDQwMjU4LCJleHAiOjE3MDU2NDUwNTh9.WIxr69oXK4GTMPGtgYq9hCULPFqv5JXT0HGNCkbUz78",
+  },
 });
