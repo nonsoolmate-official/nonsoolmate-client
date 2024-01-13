562879f090acc3eafdbd84c9c58075631d478193
SynthiaLee <seanne0801@gmail.com>
2024-01-13 23:42:21 +0900

    [ Init/#88 ] authorization env 사용

--- nonsoolmateClient/src/api/axios.ts
+++ nonsoolmateClient/src/api/axios.ts
@@ -1,11 +1,10 @@
 import axios, { AxiosInstance } from "axios";
 
 export const client: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: `${import.meta.env.VITE_BASE_URL}`,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsIm1lbWJlcklkIjoyLCJlbWFpbCI6InRoZGFsc3JiNzlAbmF2ZXIuY29tIiwiaWF0IjoxNzA1MDQwMjU4LCJleHAiOjE3MDU2NDUwNTh9.WIxr69oXK4GTMPGtgYq9hCULPFqv5JXT0HGNCkbUz78",
+    Authorization: `${import.meta.env.VITE_ACCESS_TOKEN}`,
   },
 });
