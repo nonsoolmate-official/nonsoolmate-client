73cf003431235d624a85c3f41661b73493c2b88d
HAE2NI <hae0_02ni@ewhain.net>
2024-01-13 17:42:40 +0900

    [ hotfix ] 배포 오류 잡기

--- nonsoolmateClient/vite.config.ts
+++ nonsoolmateClient/vite.config.ts
@@ -22,4 +22,15 @@ export default defineConfig({
       { find: "@type", replacement: path.resolve(__dirname, "src/type") },
     ],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
+  },
 });
