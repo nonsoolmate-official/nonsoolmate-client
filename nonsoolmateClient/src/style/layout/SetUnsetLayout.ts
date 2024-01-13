5538d19d15fc80c1cb567d073dac596a3daa9c98
Rose <120544840+Rose-my@users.noreply.github.com>
2024-01-14 02:34:18 +0900

    [ refactor/#90 ] Container의 공통 layout스타일 수정

--- nonsoolmateClient/src/style/layout/SetUnsetLayout.ts
+++ nonsoolmateClient/src/style/layout/SetUnsetLayout.ts
@@ -6,7 +6,6 @@ export const SetUnsetContainerLayout = css`
   overflow: scroll;
   width: calc(100% - 43.1rem);
   height: 100%;
-  padding: 6.7rem 21.5rem 11.5rem 2.4rem;
   background-color: ${({ theme }) => theme.colors.grey_50};
 `;
 
