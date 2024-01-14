238281d1940150391743a46ad479c6eb3ed251e1
SynthiaLee <seanne0801@gmail.com>
2024-01-14 17:46:55 +0900

    [ Feat/#93 ] 첨삭 뷰와 문제 뷰 분기처리를 위해 pdfViewerWrapper props 수정

--- nonsoolmateClient/src/answer/correction/index.tsx
+++ nonsoolmateClient/src/answer/correction/index.tsx
@@ -1,14 +1,19 @@
 import ExplainHeader from "answer/components/ExplainHeader";
 import PdfViewerWrapper from "answer/components/PdfViewerWrapper";
-
+import { useGetCorrectionPageData } from "answer/hooks/useGetCorrectionPagePdfs";
 export default function index() {
+  const id = 1;
+  const correctionRes = useGetCorrectionPageData(id);
+  console.log(correctionRes);
+
   return (
     <>
       <ExplainHeader testTitle="중앙대학교 - 2021 인문사회 1" />
       <PdfViewerWrapper
         firstTitle="첨삭"
         secondTitle="해제"
-        pdfUrl="http://www.usrap.org/sites/default/files/historical/pdf/usRAP_brochure.pdf"
+        firstPdfUrl="http://www.usrap.org/sites/default/files/historical/pdf/usRAP_brochure.pdf"
+        secondPdfUrl="https://www.sldttc.org/allpdf/21583473018.pdf"
       />
     </>
   );
