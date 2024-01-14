e27f20c110cde4ecfdfea3907796826a24e61210
Soyoung Park <soyoung008@naver.com>
2024-01-14 18:03:21 +0900

    [ feat/#98 ] 시험지 이미지 불러오는 api 타입 추가

--- nonsoolmateClient/src/takeTest/api/getUniversityExamImages.ts
+++ nonsoolmateClient/src/takeTest/api/getUniversityExamImages.ts
@@ -1,6 +1,43 @@
 import { client } from "@api/axios";
+import { Response } from "types/common";
+interface DataTypes {
+  content: ContentItem[];
+  pageable: Pageable;
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: Sort;
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+}
+interface ContentItem {
+  examImgUrl: string;
+}
+
+interface PageableSort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+
+interface Pageable {
+  pageNumber: number;
+  pageSize: number;
+  sort: PageableSort;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
 export async function getUniversityExamImages(pageNum: number) {
-  const response = await client.get(`/university/exam/1/image?page=${pageNum}`);
+  const response = await client.get<Response<DataTypes>>(`/university/exam/1/image?page=${pageNum}`);
 
   return response.data;
 }
