ee83bbb94e082ac03d9d34f007c2bf52f964fdd8
Soyoung Park <soyoung008@naver.com>
2024-01-14 17:30:09 +0900

    [ feat/#98 ] getUniversityExam 타입 추가

--- nonsoolmateClient/src/takeTest/hooks/useGetUniversityExam.ts
+++ nonsoolmateClient/src/takeTest/hooks/useGetUniversityExam.ts
@@ -2,10 +2,10 @@ import { useQuery } from "react-query";
 import { getUniversityExam } from "takeTest/api/getUniversityExam";
 
 export function useGetUniversityExam(id: number) {
-  const result = useQuery(["info", id], () => getUniversityExam(id), {
+  const { data } = useQuery(["info", id], () => getUniversityExam(id), {
     onError: (error) => {
       console.log("에러 발생", error);
     },
   });
-  return result;
+  return data;
 }
