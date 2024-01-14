f36aabdf399168109efeb47e608b4a388cdacf43
Soyoung Park <soyoung008@naver.com>
2024-01-14 17:59:47 +0900

    [ feat/#98 ] usequery 키 값 주기

--- nonsoolmateClient/src/takeTest/hooks/useGetUniversityExam.ts
+++ nonsoolmateClient/src/takeTest/hooks/useGetUniversityExam.ts
@@ -1,8 +1,12 @@
 import { useQuery } from "react-query";
 import { getUniversityExam } from "takeTest/api/getUniversityExam";
 
+const QUERY_KEY = {
+  getUniversityExam: "getUniversityExam",
+};
+
 export function useGetUniversityExam(id: number) {
-  const { data } = useQuery(["info", id], () => getUniversityExam(id), {
+  const { data } = useQuery([QUERY_KEY.getUniversityExam, id], () => getUniversityExam(id), {
     onError: (error) => {
       console.log("에러 발생", error);
     },
