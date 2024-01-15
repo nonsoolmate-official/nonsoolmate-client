2aa500169170d7b1d75dcbde7b789b98e69fbbcc
HAE2NI <hae0_02ni@ewhain.net>
2024-01-16 01:25:25 +0900

    [ hotfix ] usePatch queryClient.invalidateQueries 사용

--- nonsoolmateClient/src/home/hooks/usePatchSelectUniversities.ts
+++ nonsoolmateClient/src/home/hooks/usePatchSelectUniversities.ts
@@ -1,22 +1,17 @@
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { PatchDataTypes, patchSelectUniversities } from "home/api/patchSelectUniversities";
 
-const QUERY_KEY = {
-  patchSelectUniversities: "patchSelectUniversities",
-};
-
 export default function usePatchSelectUniversities() {
-  const { mutate } = useMutation(
-    [QUERY_KEY.patchSelectUniversities],
-    (universityIdList: PatchDataTypes[]) => patchSelectUniversities(universityIdList),
-    {
-      onError: (error) => {
-        console.log("에러 발생", error);
-      },
-      onSuccess: () => {
-        console.log("성공");
-      },
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation((universityIdList: PatchDataTypes[]) => patchSelectUniversities(universityIdList), {
+    onError: (error) => {
+      console.log("에러 발생", error);
     },
-  );
-  return mutate;
+    onSuccess: (data) => {
+      console.log("성공", data);
+      queryClient.invalidateQueries("getSelectUniversityExams");
+    },
+  });
+  return { mutate };
 }
