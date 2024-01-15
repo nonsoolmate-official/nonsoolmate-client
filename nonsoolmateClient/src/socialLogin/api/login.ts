f48914bf640a707eb98d94dae25bbe6eda2ec9cf
HAE2NI <hae0_02ni@ewhain.net>
2024-01-15 19:02:21 +0900

    [ feat ] login

---
+++ nonsoolmateClient/src/socialLogin/api/login.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export async function login(code: string) {
+  const response = await axios.post(
+    `${import.meta.env.VITE_BASE_URL}/auth/social/login`,
+    {
+      platformType: "NAVER",
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "authorization-code": code,
+      },
+    },
+  );
+
+  return response;
+}
