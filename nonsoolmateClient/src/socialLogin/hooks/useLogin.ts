17f500636615111896afd9837b87fbd7194ae647
HAE2NI <hae0_02ni@ewhain.net>
2024-01-15 19:41:28 +0900

    [ feat ] useLogin에서 accessToken cookie에 담도록 설정

--- nonsoolmateClient/src/socialLogin/hooks/useLogin.ts
+++ nonsoolmateClient/src/socialLogin/hooks/useLogin.ts
@@ -1,14 +1,16 @@
 import { useMutation, useQueryClient } from "react-query";
 import { login } from "socialLogin/api/login";
+import { setCookie } from "@api/cookie";
 
 export default function useLogin() {
   const queryClient = useQueryClient();
 
   const { mutate, ...rest } = useMutation(login, {
     onSuccess: (data) => {
-      queryClient.setQueryData("authToken", data);
-      window.location.href = "/home/test";
       console.log("성공", data);
+      setCookie("accessToken", data.accessToken, { path: "/" });
+      queryClient.setQueryData("authToken", data);
+      //   window.location.href = "/home/test";
     },
   });
   return { mutate, ...rest };
