feccd724a4756d92edb6dea3288be21d3b8af22a
HAE2NI <hae0_02ni@ewhain.net>
2024-01-15 19:01:29 +0900

    [ etc ] naverLoginCode 삭제

--- nonsoolmateClient/src/api/auth/naverLogincode.ts
+++
@@ -1,5 +0,0 @@
-//네이버 소셜 로그인 인가 코드 받는 코드입니당~
-
-export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
-  import.meta.env.VITE_CLIENT_ID
-}&state=${import.meta.env.VITE_NAVER_STATE}l&redirect_uri=${import.meta.env.VITE_CALLBACK_URL}`;
