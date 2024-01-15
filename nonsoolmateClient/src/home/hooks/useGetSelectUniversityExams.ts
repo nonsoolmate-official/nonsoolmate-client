f919636fdb3a4e4abd8502bc29c57d48189426d7
HAE2NI <hae0_02ni@ewhain.net>
2024-01-15 23:57:17 +0900

    [ feat ] useGetSelectUniversityExams

--- nonsoolmateClient/src/home/hooks/useGetSelectUniversityExams.ts
+++ nonsoolmateClient/src/home/hooks/useGetSelectUniversityExams.ts
@@ -2,12 +2,11 @@ import { getSelectUniversityExams } from "home/api/getSelectUniversityExams";
 
 import { useQuery } from "react-query";
 
-const QUERY_KEY = {
-  getSelectUniversityExams: "getSelectUniversityExams",
-};
-
 export default function useGetSelectUniversityExams() {
-  const { data } = useQuery(QUERY_KEY.getSelectUniversityExams, () => getSelectUniversityExams(), {
+  const { data } = useQuery("getSelectUniversityExams", () => getSelectUniversityExams(), {
+    onSuccess: (data) => {
+      console.log("불러와라뿅", data);
+    },
     onError: (error) => {
       console.log("에러 발생", error);
     },
