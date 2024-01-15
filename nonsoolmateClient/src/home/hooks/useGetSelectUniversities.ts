ce4483b0c665df976b2182ef827d3424555ef262
HAE2NI <hae0_02ni@ewhain.net>
2024-01-16 02:26:17 +0900

    [ hotfix ] useGetSelectUniversities querykey 변경

--- nonsoolmateClient/src/home/hooks/useGetSelectUniversities.ts
+++ nonsoolmateClient/src/home/hooks/useGetSelectUniversities.ts
@@ -1,12 +1,8 @@
 import { useQuery } from "react-query";
 import { getSelectUniversities } from "home/api/getSelectUniversities";
 
-const QUERY_KEY = {
-  getSelectUniversities: "getSelectUniversities",
-};
-
 export default function useGetSelectUniversities() {
-  const { data } = useQuery(QUERY_KEY.getSelectUniversities, () => getSelectUniversities(), {
+  const { data } = useQuery("getSelectUniversities", () => getSelectUniversities(), {
     onError: (error) => {
       console.log("에러 발생", error);
     },
