937885eea5aff00a4f1a908d0ae4a3cdea65fc32
HAE2NI <hae0_02ni@ewhain.net>
2024-01-18 02:43:02 +0900

    [ feat ] accessToken 넣어주기 header에 default로

--- nonsoolmateClient/src/api/axios.ts
+++ nonsoolmateClient/src/api/axios.ts
@@ -1,11 +1,9 @@
 import axios, { AxiosInstance } from "axios";
-import { getCookie } from "./cookie";
 
 export const client: AxiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}`,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    Authorization: `Bearer ${getCookie("accessToken")}`,
   },
 });
