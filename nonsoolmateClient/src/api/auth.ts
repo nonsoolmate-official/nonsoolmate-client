a2129c0f69de227befb76ea1a316e9828d2e3590
Soyoung Park <soyoung008@naver.com>
2024-04-03 18:18:27 +0900

    feat : sessionStorage로 변경

--- nonsoolmateClient/src/api/auth.ts
+++ nonsoolmateClient/src/api/auth.ts
@@ -1,13 +1,13 @@
 const ACCESS_TOKEN = "token";
 
 export const getToken = () => {
-  return localStorage.getItem(ACCESS_TOKEN);
+  return sessionStorage.getItem(ACCESS_TOKEN);
 };
 
 export const setToken = (token: string) => {
-  localStorage.setItem(ACCESS_TOKEN, token);
+  sessionStorage.setItem(ACCESS_TOKEN, token);
 };
 
 export const removeToken = () => {
-  localStorage.removeItem(ACCESS_TOKEN);
+  sessionStorage.removeItem(ACCESS_TOKEN);
 };
