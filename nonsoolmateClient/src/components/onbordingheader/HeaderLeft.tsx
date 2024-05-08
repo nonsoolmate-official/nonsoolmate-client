7f1a59a227766b198adef5d06222b724cdead222
Soyoung Park <soyoung008@naver.com>
2024-05-08 17:05:33 +0900

    feat : 헤더에 nonsoolmate 아이콘 클릭 시 로그인 유무에 따라 이동 다르게 되도록 구현

--- nonsoolmateClient/src/components/onbordingheader/HeaderLeft.tsx
+++ nonsoolmateClient/src/components/onbordingheader/HeaderLeft.tsx
@@ -1,11 +1,13 @@
 import { LogoIc } from "@assets/index";
+import useGetName from "home/hooks/useGetName";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
 
 export default function HeaderLeft() {
   const navigate = useNavigate();
+  const getNameResponse = useGetName();
   return (
-    <LogoContainer type="button" onClick={() => navigate("/")}>
+    <LogoContainer type="button" onClick={() => (getNameResponse ? navigate("/home/test") : navigate("/"))}>
       <LogoIcon />
     </LogoContainer>
   );
