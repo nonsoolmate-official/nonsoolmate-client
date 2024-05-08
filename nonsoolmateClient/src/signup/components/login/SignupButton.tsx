0776f3d9be51e74b1ca85c864b8184c20c4c5a7a
Soyoung Park <soyoung008@naver.com>
2024-05-08 17:15:32 +0900

    feat : 회원가입 버튼에도 네이버 인증 페이지 연결 추가

--- nonsoolmateClient/src/signup/components/login/SignupButton.tsx
+++ nonsoolmateClient/src/signup/components/login/SignupButton.tsx
@@ -1,7 +1,15 @@
+import { redirectToNaverAuth } from "socialLogin/utils/redirectToNaverAuth";
 import styled from "styled-components";
 
 export default function SignupButton() {
-  return <Button type="button">네이버로 회원가입하기</Button>;
+  function loginHandler() {
+    redirectToNaverAuth();
+  }
+  return (
+    <Button onClick={loginHandler} type="button">
+      네이버로 회원가입하기
+    </Button>
+  );
 }
 
 const Button = styled.button`
