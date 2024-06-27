327b6da297c566f6d50868a65edb5a6761b2fe72
Rose <120544840+Rose-my@users.noreply.github.com>
2024-06-28 00:59:54 +0900

    design: 온보딩 Banner

--- nonsoolmateClient/src/onboarding/components/banner/HomeImg.tsx
+++ nonsoolmateClient/src/onboarding/components/banner/HomeImg.tsx
@@ -1,5 +1,6 @@
 import HomeTestSet from "@assets/image/homeTestSet.png";
 import styled from "styled-components";
+import { media } from "style/responsiveStyle";
 
 export default function HomeImg() {
   return <Image src={HomeTestSet} alt="메인홈사진" />;
@@ -8,4 +9,9 @@ export default function HomeImg() {
 const Image = styled.img`
   width: 87.9rem;
   height: 33.4rem;
+
+  ${media.tablet} {
+    width: 70.2rem;
+    height: 31rem;
+  }
 `;
