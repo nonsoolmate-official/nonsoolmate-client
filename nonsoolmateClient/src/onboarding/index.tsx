d8a171ad3f6d3b915c2864178fc64f800f7090aa
Rose <120544840+Rose-my@users.noreply.github.com>
2024-06-29 02:23:34 +0900

    design: 헤더 아이패드UI

--- nonsoolmateClient/src/onboarding/index.tsx
+++ nonsoolmateClient/src/onboarding/index.tsx
@@ -4,11 +4,17 @@ import FaQ from "./components/faq/FaQ";
 import Footer from "./components/footer/Footer";
 import Header from "../components/onbordingheader/Header";
 import Univeristy from "./components/univeristy/Univeristy";
+import HomeHeader from "home/components/HomeHeader";
+import { getToken } from "socialLogin/utils/token";
+import useGetName from "home/hooks/useGetName";
 
 export default function OnBoarding() {
+  const token = getToken();
+  const getNameResponse = token ? useGetName() : null;
+
   return (
     <>
-      <Header isOnboarding={true} />
+      {getNameResponse ? <HomeHeader /> : <Header isOnboarding={false} />}
       <Banner />
       <Univeristy />
       <Advertise />
