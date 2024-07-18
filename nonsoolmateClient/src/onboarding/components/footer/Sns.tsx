da532be3b499ee8db996c7c18271936b1135cbf0
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-18 17:32:15 +0900

    feat: url 연결

--- nonsoolmateClient/src/onboarding/components/footer/Sns.tsx
+++ nonsoolmateClient/src/onboarding/components/footer/Sns.tsx
@@ -2,10 +2,20 @@ import { InstaIc, KakaoIc } from "@assets/index";
 import styled from "styled-components";
 
 export default function Sns() {
+  const openUrl = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <Container>
-      <InstaIcon />
-      <KakaoIcon />
+      <BtnWrapper type="button" onClick={() => openUrl("http://pf.kakao.com/_iuHpG")}>
+        <KakaoIcon />
+      </BtnWrapper>
+      <BtnWrapper
+        type="button"
+        onClick={() => openUrl("https://www.instagram.com/nonsoolmate?igsh=amFiemthdmRxMDg4&utm_source=qr")}>
+        <InstaIcon />
+      </BtnWrapper>
     </Container>
   );
 }
@@ -24,4 +34,11 @@ const KakaoIcon = styled(KakaoIc)`
 const InstaIcon = styled(InstaIc)`
   width: 3.6rem;
   height: 3.6rem;
+`;
+
+const BtnWrapper = styled.button`
+  padding: 0;
+  border: none;
+  background: none;
+  cursor: pointer;
 `;
