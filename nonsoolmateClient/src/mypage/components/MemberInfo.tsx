96c6b36988f1d6ee19ffae6ca94198f939d608e4
Rose <120544840+Rose-my@users.noreply.github.com>
2024-08-06 13:39:15 +0900

    design: MemberInfo 포지션값 수정

--- nonsoolmateClient/src/mypage/components/MemberInfo.tsx
+++ nonsoolmateClient/src/mypage/components/MemberInfo.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import useGetProfile from "mypage/hooks/useGetProfile";
+import { media } from "style/responsiveStyle";
 
 export default function MemberInfo() {
   const response = useGetProfile();
@@ -38,39 +39,57 @@ const MemberInfoContainer = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 2.8rem;
+  position: relative;
   width: 100%;
   padding: 8rem 6.4rem;
+
+  ${media.tablet} {
+    padding: 3.1rem 3.2rem;
+  }
 `;
+
 const Info = styled.li`
   display: flex;
   gap: 0.2rem;
-  position: relative;
   width: 65rem;
 `;
+
 const Title = styled.p`
   ${({ theme }) => theme.fonts.Body3};
 
   width: 8.8rem;
 `;
+
 const Text = styled.p`
   ${({ theme }) => theme.fonts.Body4};
 `;
+
 const ReviseButton = styled.button`
   ${({ theme }) => theme.fonts.Body4};
 
   position: absolute;
-  right: 0;
+  right: 22.5rem;
   width: 7.6rem;
   color: ${({ theme }) => theme.colors.main_blue};
+
+  ${media.tablet} {
+    right: 3.2rem;
+  }
 `;
+
 const GenderBox = styled.ul`
   ${({ theme }) => theme.fonts.Body7};
 
   display: flex;
   gap: 0.6rem;
   position: absolute;
-  right: 26rem;
+  right: 48.4rem;
+
+  ${media.tablet} {
+    right: 3.2rem;
+  }
 `;
+
 const Male = styled.li<{ $gender: String }>`
   padding: 0.4rem 0.8rem;
   border: 1px solid ${({ $gender, theme }) => ($gender == "M" ? theme.colors.main_blue : theme.colors.grey_400)};
@@ -78,6 +97,7 @@ const Male = styled.li<{ $gender: String }>`
   background-color: ${({ $gender, theme }) => ($gender == "M" ? theme.colors.light_blue : theme.colors.grey_50)};
   color: ${({ $gender, theme }) => ($gender == "M" ? theme.colors.main_blue : theme.colors.grey_400)};
 `;
+
 const Female = styled.li<{ $gender: String }>`
   padding: 0.4rem 0.8rem;
   border: 1px solid ${({ $gender, theme }) => ($gender == "F" ? theme.colors.main_blue : theme.colors.grey_400)};
