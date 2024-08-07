5089bf21dcab6ad3718ad42235072dbc6d6d8105
Rose <120544840+Rose-my@users.noreply.github.com>
2024-08-07 18:52:45 +0900

    feat: 인쇄, 다운로드 플러그인 추가

--- nonsoolmateClient/src/answer/components/PdfViewer.tsx
+++ nonsoolmateClient/src/answer/components/PdfViewer.tsx
@@ -5,6 +5,8 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import { GetFilePlugin } from "@react-pdf-viewer/get-file";
 import { FullScreenPlugin } from "@react-pdf-viewer/full-screen";
 import { media } from "style/responsiveStyle";
+import { printPlugin, RenderPrintProps, PrintIcon } from "@react-pdf-viewer/print";
+import { getFilePlugin, RenderDownloadProps, DownloadIcon } from "@react-pdf-viewer/get-file";
 
 interface PdfViewerProps {
   pdfUrl: string;
@@ -15,31 +17,84 @@ interface PdfViewerProps {
 }
 
 export default function PdfViewer(props: PdfViewerProps) {
-  const { pdfUrl, getFilePluginInstance, fullScreenPluginInstance, selectTest, selectExplanation } = props;
+  const { pdfUrl, fullScreenPluginInstance, selectTest, selectExplanation } = props;
 
-  let plugins: Plugin[] | undefined = [];
-  if (getFilePluginInstance && fullScreenPluginInstance) {
-    plugins = [getFilePluginInstance, fullScreenPluginInstance];
-  } else if (getFilePluginInstance) {
-    plugins = [getFilePluginInstance];
-  } else if (fullScreenPluginInstance) {
-    plugins = [fullScreenPluginInstance];
-  }
+  // let plugins: Plugin[] | undefined = [];
+  // if (getFilePluginInstance && fullScreenPluginInstance) {
+  //   plugins = [getFilePluginInstance, fullScreenPluginInstance];
+  // } else if (getFilePluginInstance) {
+  //   plugins = [getFilePluginInstance];
+  // } else if (fullScreenPluginInstance) {
+  //   plugins = [fullScreenPluginInstance];
+  // }
+
+  const printPluginInstance = printPlugin();
+  const { Print } = printPluginInstance;
+  const getFilePluginInstance = getFilePlugin();
+  const { Download } = getFilePluginInstance;
 
   return (
-    <PdfViewerWrapper $selectTest={selectTest} $selectExplanation={selectExplanation}>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-        <ViewerWrapper>
-          <Viewer fileUrl={pdfUrl} theme={{ theme: "light" }} plugins={plugins} />
-        </ViewerWrapper>
-      </Worker>
-    </PdfViewerWrapper>
+    <>
+      <div
+        style={{
+          alignItems: "center",
+          borderBottom: "1px solid black",
+          display: "flex",
+          padding: "4px",
+          gap: "20px",
+        }}>
+        <Print>
+          {(props: RenderPrintProps) => (
+            <button
+              style={{
+                top: "10rem",
+                border: "none",
+                color: "black",
+                cursor: "pointer",
+                padding: 0,
+                margin: 0,
+              }}
+              onClick={props.onClick}>
+              <PrintIcon />
+            </button>
+          )}
+        </Print>
+        <Download>
+          {(props: RenderDownloadProps) => (
+            <button
+              style={{
+                top: "10rem",
+                border: "none",
+                color: "black",
+                cursor: "pointer",
+                padding: 0,
+                margin: 0,
+              }}
+              onClick={props.onClick}>
+              <DownloadIcon />
+            </button>
+          )}
+        </Download>
+      </div>
+      <PdfViewerWrapper $selectTest={selectTest} $selectExplanation={selectExplanation}>
+        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`}>
+          <ViewerWrapper>
+            <Viewer
+              fileUrl={pdfUrl}
+              theme={{ theme: "light" }}
+              plugins={[printPluginInstance, getFilePluginInstance]}
+            />
+          </ViewerWrapper>
+        </Worker>
+      </PdfViewerWrapper>
+    </>
   );
 }
 
 const PdfViewerWrapper = styled.div<{ $selectTest?: boolean; $selectExplanation?: boolean }>`
   ${({ theme }) => theme.effects.pdf_shadow};
 
+  overflow: auto;
   width: 100%;
   height: calc(100vh - 16.4rem);
   border-radius: 8px;
