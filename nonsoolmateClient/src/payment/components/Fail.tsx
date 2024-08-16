cc92ad8fccb9b87f314405b47af4c19531bf52c3
Soyoung Park <soyoung008@naver.com>
2024-08-17 00:19:45 +0900

    feat : 리다이렉트 페이지 생성

--- nonsoolmateClient/src/payment/components/Fail.tsx
+++ nonsoolmateClient/src/payment/components/Fail.tsx
@@ -1,3 +1,15 @@
+import { useSearchParams } from "react-router-dom";
+
 export default function Fail() {
-  return <></>;
+  const [searchParams] = useSearchParams();
+
+  return (
+    <div className="result wrapper">
+      <div className="box_section">
+        <h2>결제 실패</h2>
+        <p>{`에러 코드: ${searchParams.get("code")}`}</p>
+        <p>{`실패 사유: ${searchParams.get("message")}`}</p>
+      </div>
+    </div>
+  );
 }
