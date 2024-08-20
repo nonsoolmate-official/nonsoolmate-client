ed08cec266f7aa1598820a1a0369fd40187dc5a0
Rose <120544840+Rose-my@users.noreply.github.com>
2024-08-20 18:00:23 +0900

    design: PdfViewerWrapper

--- nonsoolmateClient/src/answer/components/PdfViewerWrapper.tsx
+++ nonsoolmateClient/src/answer/components/PdfViewerWrapper.tsx
@@ -1,10 +1,7 @@
 import { columnFlex, commonFlex } from "style/commonStyle";
 import PdfViewer from "./PdfViewer";
-import TitleWrapper from "./TitleWrapper";
 import styled from "styled-components";
-import { getFilePlugin } from "@react-pdf-viewer/get-file";
 import { useEffect, useState } from "react";
-import { fullScreenPlugin } from "@react-pdf-viewer/full-screen";
 import { RightArrowBigIc, LeftArrowBigIc, ShowExplanationTagIc, ShowQuestionTagIc } from "@assets/index";
 import { media } from "style/responsiveStyle";
 
@@ -19,8 +16,6 @@ interface PdfViewerWrapperProps {
 
 export default function PdfViewerWrapper(props: PdfViewerWrapperProps) {
   const { firstTitle, secondTitle, ifExplanation, firstPdfUrl, secondPdfUrl, testUrl } = props;
-  const getFilePluginInstance = getFilePlugin();
-  const fullScreenPluginInstance = fullScreenPlugin();
   const [isQuestionHide, setIsQuestionHide] = useState(false);
   const [isExplanationHide, setIsExplanationHide] = useState(false);
   const [isHide, setIsHide] = useState(false);
@@ -28,6 +23,14 @@ export default function PdfViewerWrapper(props: PdfViewerWrapperProps) {
 
   const [selectTest, setSelectTest] = useState(true);
   const [selectExplanation, setSelectExplanation] = useState(false);
+
+  useEffect(() => {
+    if (isExplanationHide || isQuestionHide) {
+      setIsHide(true);
+    } else {
+      setIsHide(false);
+    }
+  }, [isExplanationHide, isQuestionHide]);
 
   function clickTestButton() {
     if (selectTest && !selectExplanation) {
@@ -44,14 +47,6 @@ export default function PdfViewerWrapper(props: PdfViewerWrapperProps) {
     }
   }
 
-  useEffect(() => {
-    if (isExplanationHide || isQuestionHide) {
-      setIsHide(true);
-    } else {
-      setIsHide(false);
-    }
-  }, [isExplanationHide, isQuestionHide]);
-
   return (
     <PdfViewerContainer $isHide={isHide}>
       <ShowQuestionButton
@@ -64,6 +59,8 @@ export default function PdfViewerWrapper(props: PdfViewerWrapperProps) {
           <ShowQuestionTagIcon />
         </ShowQuestionMessageBox>
       </ShowQuestionButton>
+
+      {/* -------아이패드 반응형 헤더-------  */}
       <IpadButtonContainer>
         <TestButton type="button" onClick={clickTestButton} $selectTest={selectTest}>
           {firstTitle}
@@ -72,22 +69,23 @@ export default function PdfViewerWrapper(props: PdfViewerWrapperProps) {
           {secondTitle}
         </ExplanationButton>
       </IpadButtonContainer>
+
       <IpadPdfViewer>
         {selectTest && (
           <>
             {ifExplanation && testUrl ? (
               <PdfViewer
                 pdfUrl={testUrl}
-                getFilePluginInstance={getFilePluginInstance}
                 selectTest={selectTest}
                 selectExplanation={selectExplanation}
+                setIsHide={setIsQuestionHide}
               />
             ) : (
               <PdfViewer
                 pdfUrl={firstPdfUrl}
-                getFilePluginInstance={getFilePluginInstance}
                 selectTest={selectTest}
                 selectExplanation={selectExplanation}
+                setIsHide={setIsQuestionHide}
               />
             )}
           </>
@@ -95,40 +93,42 @@ export default function PdfViewerWrapper(props: PdfViewerWrapperProps) {
         {selectExplanation && (
           <PdfViewer
             pdfUrl={secondPdfUrl ? secondPdfUrl : firstPdfUrl}
-            fullScreenPluginInstance={fullScreenPluginInstance}
             selectTest={selectTest}
             selectExplanation={selectExplanation}
+            setIsHide={setIsExplanationHide}
           />
         )}
       </IpadPdfViewer>
 
       <LeftPdfViewerWrapper $isQuestionHide={isQuestionHide} $isExplanationHide={isExplanationHide}>
-        <TitleWrapper
-          title={firstTitle}
-          buttonText={ifExplanation ? "문제 숨기기" : "첨삭 PDF로 저장"}
-          ifPdfButton={!ifExplanation && true}
-          getFilePluginInstance={getFilePluginInstance}
-          setIsHide={setIsQuestionHide}
-        />
         {ifExplanation && testUrl ? (
-          <PdfViewer pdfUrl={testUrl} getFilePluginInstance={getFilePluginInstance} />
+          <PdfViewer
+            pdfUrl={testUrl}
+            title={firstTitle}
+            buttonText={ifExplanation ? "문제 숨기기" : "첨삭 PDF로 저장"}
+            ifPdfButton={!ifExplanation && true}
+            setIsHide={setIsQuestionHide}
+          />
         ) : (
-          <PdfViewer pdfUrl={firstPdfUrl} getFilePluginInstance={getFilePluginInstance} />
+          <PdfViewer
+            pdfUrl={firstPdfUrl}
+            title={firstTitle}
+            buttonText={ifExplanation ? "문제 숨기기" : "첨삭 PDF로 저장"}
+            ifPdfButton={!ifExplanation && true}
+            setIsHide={setIsQuestionHide}
+          />
         )}
       </LeftPdfViewerWrapper>
 
       <RightPdfViewerWrapper $isQuestionHide={isQuestionHide} $isExplanationHide={isExplanationHide}>
-        <TitleWrapper
+        <PdfViewer
+          pdfUrl={secondPdfUrl ? secondPdfUrl : firstPdfUrl}
           title={secondTitle}
           buttonText="해제 숨기기"
           ifExplanation={ifExplanation}
           setIsHide={setIsExplanationHide}
-          fullScreenPluginInstance={fullScreenPluginInstance}
-          isQuestionHide={isQuestionHide}
-        />
-        <PdfViewer
-          pdfUrl={secondPdfUrl ? secondPdfUrl : firstPdfUrl}
-          fullScreenPluginInstance={fullScreenPluginInstance}
+          selectExplanation={false}
+          selectTest={false}
         />
       </RightPdfViewerWrapper>
       <ShowExplanationButton
@@ -180,36 +180,15 @@ const PdfViewerContainer = styled.section<{ $isHide: boolean }>`
   ${media.tablet} {
     ${columnFlex}
 
-    gap: 1.4rem;
     padding: 2rem 3rem;
   }
 `;
 
-const IpadButtonContainer = styled.div`
-  ${media.tablet} {
-    display: flex;
-    gap: 2.4rem;
-    width: 100%;
-    height: 7rem;
-    ${({ theme }) => theme.fonts.Headline3}
-  }
-
-  display: none;
-`;
-
-const TestButton = styled.button<{ $selectTest: boolean }>`
-  padding: 0;
-  color: ${({ theme, $selectTest }) => ($selectTest ? theme.colors.black : theme.colors.grey_300)};
-`;
-const ExplanationButton = styled.button<{ $selectExplanation: boolean }>`
-  padding: 0;
-  color: ${({ theme, $selectExplanation }) => ($selectExplanation ? theme.colors.black : theme.colors.grey_300)};
-`;
 const IpadPdfViewer = styled.article`
   ${media.tablet} {
     display: flex;
     flex-direction: column;
-    gap: 1.4rem;
+    gap: 6rem;
     width: 100%;
   }
 
@@ -267,4 +246,26 @@ const ShowExplanationButton = styled.button<{ $isExplanationHide: boolean }>`
   display: ${({ $isExplanationHide }) => ($isExplanationHide ? `block` : `none`)};
   position: fixed;
   right: 4.8rem;
+`;
+
+const IpadButtonContainer = styled.div`
+  ${media.tablet} {
+    display: flex;
+    gap: 2.4rem;
+    width: 100%;
+    height: 7rem;
+    ${({ theme }) => theme.fonts.Headline3}
+  }
+
+  display: none;
+`;
+
+const TestButton = styled.button<{ $selectTest: boolean }>`
+  padding: 0;
+  color: ${({ theme, $selectTest }) => ($selectTest ? theme.colors.black : theme.colors.grey_300)};
+`;
+
+const ExplanationButton = styled.button<{ $selectExplanation: boolean }>`
+  padding: 0;
+  color: ${({ theme, $selectExplanation }) => ($selectExplanation ? theme.colors.black : theme.colors.grey_300)};
 `;
