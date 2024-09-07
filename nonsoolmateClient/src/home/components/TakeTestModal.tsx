af8039d1fd2efbad5f7e9a77c188825dbd63d32c
Rose <120544840+Rose-my@users.noreply.github.com>
2024-09-07 15:45:16 +0900

    etc: 머지 에러 해결

--- nonsoolmateClient/src/home/components/TakeTestModal.tsx
+++ nonsoolmateClient/src/home/components/TakeTestModal.tsx
@@ -1,16 +1,11 @@
 import styled from "styled-components";
 import { commonFlex, takeTestModalNextButtonStyle } from "style/commonStyle";
-
 import { media } from "style/responsiveStyle";
 import { DisplayIc, EmptyCircleIc, FilledCircleIc, PrintIc, XIc } from "@assets/index";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { takeTestPdfPlugin } from "recoil/atom";
-
-import { useRecoilState } from "recoil";
-import { takeTestPdfPlugin } from "recoil/atom";
-
 interface TakeTestModalProps {
   examId: number;
   handleTakeTestModal: () => void;
@@ -22,8 +17,6 @@ export default function TakeTestModal(props: TakeTestModalProps) {
   const navigate = useNavigate();
   const [isDisplayClicked, setIsDisplayClicked] = useState<boolean>(false);
   const [isPrintClicked, setIsPrintClicked] = useState<boolean>(false);
-  const [, setPdfPlugin] = useRecoilState(takeTestPdfPlugin);
-
   const [, setPdfPlugin] = useRecoilState(takeTestPdfPlugin);
 
   function handleDisplayOption() {
