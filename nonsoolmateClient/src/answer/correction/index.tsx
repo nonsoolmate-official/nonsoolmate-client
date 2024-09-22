aeaedf6405c8ccb598f4f341647524707b462374
Rose <120544840+Rose-my@users.noreply.github.com>
2024-09-22 23:15:06 +0900

    fix: 500에러 "재첨삭 완료" 일때만 데이터 패칭

--- nonsoolmateClient/src/answer/correction/index.tsx
+++ nonsoolmateClient/src/answer/correction/index.tsx
@@ -2,44 +2,41 @@ import CorrectionContainer from "answer/components/correction/CorrectionContaine
 import ExplainHeader from "answer/components/explanation/ExplainHeader";
 import { useGetEditingResult } from "answer/hooks/useGetEditingResult";
 import { useGetRevisionResult } from "answer/hooks/useGetRevisionResult";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import useRefreshPage from "socialLogin/hooks/useRefreshPage";
+
 export default function index() {
   useRefreshPage();
   const location = useLocation();
-  const { examId, examName } = location.state;
+  const { examId, examName, examStatus } = location.state;
+  const [revisionStatus, setRevisionStatus] = useState(examStatus);
+  const [revisionPdfUrl, setRevisionPdfUrl] = useState("");
 
   // 첨삭 결과
   const editingRes = useGetEditingResult(examId);
 
-  //재첨삭 결과
-  const { data: revisionRes, refetch } = useGetRevisionResult(examId);
+  // 재첨삭 결과
+  const { data: revisionRes } = useGetRevisionResult(examId, examStatus);
 
-  //revisionStatus가 바뀔 때마다 refetch
   useEffect(() => {
-    refetch();
-  }, [revisionRes, refetch]);
+    if (revisionRes) {
+      setRevisionPdfUrl(revisionRes.examResultFileUrl);
+      setRevisionStatus(revisionRes.examResultStatus);
+    }
+  }, [revisionRes]);
 
   if (!editingRes) return <></>;
-  if (!revisionRes) return <></>;
-
   const editingResultFileUrl = editingRes.examResultFileUrl;
-  const revisionResultFileUrl = revisionRes.examResultFileUrl;
-  const revisionStatus = revisionRes.examResultStatus;
-
-  console.log("editingRes" + editingRes);
-  console.log("revisionRes" + revisionRes);
 
   return (
     <>
       <ExplainHeader testTitle={examName} />
       <CorrectionContainer
         editingTitle="첨삭"
-        revisionTitle="재첨삭"
-        revisionStatus={revisionStatus}
         editingPdfUrl={editingResultFileUrl}
-        revisionPdfUrl={revisionResultFileUrl}
+        revisionStatus={revisionStatus}
+        revisionPdfUrl={revisionPdfUrl}
       />
     </>
   );
--- nonsoolmateClient/src/answer/hooks/useGetRevisionResult.ts
+++ nonsoolmateClient/src/answer/hooks/useGetRevisionResult.ts
@@ -7,7 +7,7 @@ const QUERY_KEY = {
 
 export function useGetRevisionResult(examId: number, examStatus: string) {
   const { data, refetch } = useQuery([QUERY_KEY.getRevisionResult, examId], () => getRevisionResult(examId), {
-    enabled: examStatus != "첨삭 완료", // 첨삭 완료는 404에러이기 때문에 아닐때만 통신
+    enabled: examStatus === "재첨삭 완료", // 재첨삭 완료일때만 통신
     refetchInterval: 3000, // 3초마다 데이터를 refetch
     onError: (error) => {
       console.log("에러", error);
