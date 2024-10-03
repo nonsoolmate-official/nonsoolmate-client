a0f2e37289a44b08ef7c2b5b90322b6115afcda7
namdaeun <nde40345@gmail.com>
2024-10-03 16:11:21 +0900

    style : input 컴포넌트 스타일 수정

--- nonsoolmateClient/src/components/input/Input.tsx
+++ nonsoolmateClient/src/components/input/Input.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { InputHTMLAttributes } from "react";
 import styled from "styled-components";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -11,34 +11,48 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export default function Input({ placeholder, value, onChange, isError = false, errorMessage, ...props }: InputProps) {
   return (
-    <>
-      <InputLayout placeholder={placeholder} value={value} onChange={onChange} {...props} />
-      {isError && <ErrorMessage>{errorMessage}</ErrorMessage>}
-    </>
+    <InputWrapper>
+      <InputLayout placeholder={placeholder} value={value} onChange={onChange} isError={isError} {...props} />
+      <ErrorMessage isVisible={isError}>{errorMessage}</ErrorMessage>
+    </InputWrapper>
   );
 }
 
-const InputLayout = styled.input`
+const InputWrapper = styled.div`
   position: relative;
+  display: flex;
 
-  width: 100%;
+  flex-direction: column;
+
+  gap: 0.2rem;
+`;
+
+const InputLayout = styled.input<{ isError: boolean }>`
+  position: relative;
 
   padding: 0 1.2rem;
 
-  border: 1px solid ${({ theme }) => theme.colors.grey_100};
+  height: 3.6rem;
+
+  border: 1px solid ${({ theme, isError }) => (isError ? theme.colors.error : theme.colors.grey_100)};
   border-radius: 6px;
+
+  outline: none;
 
   &:focus {
     border: 1px solid ${({ theme }) => theme.colors.main_blue};
   }
 `;
 
-const ErrorMessage = styled.p`
+const ErrorMessage = styled.p<{ isVisible: boolean }>`
   position: absolute;
+  display: ${({ isVisible }) => (isVisible ? "block" : "none")};
 
-  top: 0.2rem;
+  top: 4.2rem;
+  left: 0;
 
   color: ${({ theme }) => theme.colors.error};
-
   ${({ theme }) => theme.fonts.Body8};
+
+  white-space: nowrap;
 `;
