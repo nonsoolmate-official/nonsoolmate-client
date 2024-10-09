c361a7a63c32b9885a54ef18c19d4091596275aa
Soyoung Park <soyoung008@naver.com>
2024-10-09 18:17:15 +0900

    feat : success url 에서 카드 등록 hook 호출

--- nonsoolmateClient/src/pages/payment/components/Success.tsx
+++ nonsoolmateClient/src/pages/payment/components/Success.tsx
@@ -1,52 +1,41 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { getToken } from "../../socialLogin/utils/token";
+import { usePostCardRegister } from "../hooks/usePostCardRegister";
+import Loading from "@pages/loading";
 
 export default function Success() {
   const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const customerKey = searchParams.get("customerKey");
+  const authKey = searchParams.get("authKey");
+  const id = searchParams.get("id");
+  const { mutate: postMutate } = usePostCardRegister();
 
   useEffect(() => {
-    // 쿼리 파라미터 값이 결제 요청할 때 보낸 데이터와 동일한지 반드시 확인하세요.
-    // 클라이언트에서 결제 금액을 조작하는 행위를 방지할 수 있습니다.
-    const requestData = {
-      orderId: searchParams.get("orderId"),
-      amount: searchParams.get("amount"),
-      paymentKey: searchParams.get("paymentKey"),
-    };
+    if (!customerKey || !authKey) return;
 
-    const token = getToken();
-    async function confirm() {
-      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/confirm`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+    postMutate(
+      { authKey: authKey },
+      {
+        onSuccess: () => {
+          console.log("카드 등록 성공");
+          navigate(`/payment`, { state: { id: Number(id) } });
         },
-        body: JSON.stringify(requestData),
-      });
+        onError: (error) => {
+          console.error("카드 등록 실패", error);
+        },
+      },
+    );
+  }, [authKey, navigate, postMutate]);
 
-      const json = await response.json();
-
-      if (!response.ok) {
-        // 결제 실패 비즈니스 로직을 구현하세요.
-        navigate(`/fail?message=${json.message}&code=${json.code}`);
-        return;
-      }
-
-      // 결제 성공 비즈니스 로직을 구현하세요.
-    }
-    confirm();
-  }, []);
+  if (!authKey) {
+    return <></>;
+  }
 
   return (
-    <div className="result wrapper">
-      <div className="box_section">
-        <h2>결제 성공</h2>
-        <p>{`주문번호: ${searchParams.get("orderId")}`}</p>
-        <p>{`결제 금액: ${Number(searchParams.get("amount")).toLocaleString()}원`}</p>
-        <p>{`paymentKey: ${searchParams.get("paymentKey")}`}</p>
-      </div>
-    </div>
+    <>
+      <Loading />
+    </>
   );
 }
