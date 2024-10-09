f6b70c2613590dc25d40a607667e4d29937790c0
Soyoung Park <soyoung008@naver.com>
2024-10-09 10:13:45 +0900

    Merge branch 'develop' of https://github.com/nonsoolmate-official/NONSOOLMATE-CLIENT into design/#90/Payment/UI

--- nonsoolmateClient/src/assets/index.ts
+++ nonsoolmateClient/src/assets/index.ts
@@ -1,6 +1,6 @@
 import DownArrowBoldIc from "./icon/downArrowBoldIc.svg?react";
-import DownArrowIc from "./icon/downArrowIc.svg?react";
 import DownArrowGreyIc from "./icon/downArrowGreyIc.svg?react";
+import DownArrowIc from "./icon/downArrowIc.svg?react";
 import DownArrowWhiteIc from "./icon/downArrowWhiteIc.svg?react";
 
 import LeftArrowBigIc from "./icon/leftArrowBigIc.svg?react";
@@ -10,19 +10,19 @@ import LeftCircleIc from "./icon/leftCircleIc.svg?react";
 import LeftCircleLongIc from "./icon/leftCircleLongIc.svg?react";
 
 import PrecautionNumOneIc from "./icon/precautionNumOneIc.svg?react";
-import PrecautionNumTwoIc from "./icon/precautionNumTwoIc.svg?react";
 import PrecautionNumThreeIc from "./icon/precautionNumThreeIc.svg?react";
+import PrecautionNumTwoIc from "./icon/precautionNumTwoIc.svg?react";
 
 import RightArrowBigIc from "./icon/rightArrowBigIc.svg?react";
+import RightArrowBlueIc from "./icon/rightArrowBlueIc.svg?react";
 import RightArrowIc from "./icon/rightArrowIc.svg?react";
 import RightArrowWhiteIc from "./icon/rightArrowWhiteIc.svg?react";
 import RightCircleIc from "./icon/rightCircleIc.svg?react";
-import RightArrowBlueIc from "./icon/rightArrowBlueIc.svg?react";
 
 import UpArrowBoldIc from "./icon/upArrowBoldIc.svg?react";
+import UpArrowGreyIc from "./icon/upArrowGreyIc.svg?react";
 import UpArrowIc from "./icon/upArrowIc.svg?react";
 import UpCircleIc from "./icon/upCircleIc.svg?react";
-import UpArrowGreyIc from "./icon/upArrowGreyIc.svg?react";
 
 import PracticeActiveIc from "./icon/practiceActiveIc.svg?react";
 import PracticeDisabledIc from "./icon/practiceDisabledIc.svg?react";
@@ -33,8 +33,8 @@ import StudyDisabledIc from "./icon/studyDisabledIc.svg?react";
 import TestActiveIc from "./icon/testActiveIc.svg?react";
 import TestDisabledIc from "./icon/testDisabledIc.svg?react";
 
-import LogoIc from "./icon/logoIc.svg?react";
 import FooterLogoIc from "./icon/footerLogoIc.svg?react";
+import LogoIc from "./icon/logoIc.svg?react";
 
 import LoginInfoIc from "./icon/loginInfoIc.svg?react";
 
@@ -52,14 +52,14 @@ import FindTestIc from "./icon/findTestIc.svg?react";
 import SearchIc from "./icon/searchIc.svg?react";
 
 import CheckBtnIc from "./icon/checkBtnIc.svg?react";
-import NotCheckBtnIc from "./icon/notCheckBtnIc.svg?react";
 import CheckCircleBtnIc from "./icon/checkCircleIc.svg?react";
+import NotCheckBtnIc from "./icon/notCheckBtnIc.svg?react";
 
-import SignupIc from "./icon/signupIc.svg?react";
 import NaverLoginIc from "./icon/naverLoginIc.svg?react";
+import SignupIc from "./icon/signupIc.svg?react";
 
-import QnaToogleOpenIc from "./icon/qnatoogleopenIc.svg?react";
 import QnaToogleCloseIc from "./icon/qnatooglecloseIc.svg?react";
+import QnaToogleOpenIc from "./icon/qnatoogleopenIc.svg?react";
 
 import BasicPlanIc from "./icon/basicPlanIc.svg?react";
 import PremiumPlanIc from "./icon/premiumPlanIc.svg?react";
@@ -78,8 +78,8 @@ import PrintIc from "./icon/printIc.svg?react";
 
 import EmptyCircleIc from "./icon/emptyCircleIc.svg?react";
 import FilledCircleIc from "./icon/filledCircleIc.svg?react";
-import RadioButtonDefaultIc from "./icon/radioBtnDefault.svg?react";
 import RadioButtonCheckIc from "./icon/radioBtnCheck.svg?react";
+import RadioButtonDefaultIc from "./icon/radioBtnDefault.svg?react";
 
 import DownloadCircleIc from "./icon/downloadCircleIc.svg?react";
 import PrintCircleIc from "./icon/printCircleIc.svg?react";
@@ -105,81 +105,88 @@ import Univ7Ic from "./icon/univ7Ic.svg?react";
 import Univ8Ic from "./icon/univ8Ic.svg?react";
 import Univ9Ic from "./icon/univ9Ic.svg?react";
 
-import TakeTestIc from "./icon/takeTestIc.svg?react";
 import PracticeIc from "./icon/practiceIc.svg?react";
+import TakeTestIc from "./icon/takeTestIc.svg?react";
 
-import PlusIc from "./icon/plusIc.svg?react";
 import CloseIc from "./icon/closeIc.svg?react";
-
+import CouponIc from "./icon/couponIc.svg?react";
 import CheckEmptyIc from "./icon/checkEmptyBtnIc.svg?react";
+import DiscountIc from "./icon/discountIc.svg?react";
+import MembershipIc from "./icon/membershipPlanIc.svg?react";
+import PlusIc from "./icon/plusIc.svg?react";
 
 export {
-  DownArrowBoldIc,
-  DownArrowIc,
-  LeftArrowBigIc,
-  LeftArrowBlackBtn,
-  LeftArrowWhiteIc,
-  LeftCircleIc,
-  LeftCircleLongIc,
-  PrecautionNumOneIc,
-  PrecautionNumTwoIc,
-  PrecautionNumThreeIc,
-  RightArrowBigIc,
-  RightArrowIc,
-  RightArrowWhiteIc,
-  RightCircleIc,
-  UpArrowBoldIc,
-  UpArrowIc,
-  UpCircleIc,
-  PracticeActiveIc,
-  PracticeDisabledIc,
-  StudyActiveIc,
-  StudyDisabledIc,
-  TestActiveIc,
-  TestDisabledIc,
-  LogoIc,
-  LoginInfoIc,
-  BorderIc,
-  InstaIc,
-  RightWhiteArrowIc,
-  EmptyIc,
-  HomeTestUnsetIc,
-  FindTestIc,
-  RightArrowBlueIc,
-  SearchIc,
-  CheckBtnIc,
-  NotCheckBtnIc,
-  SignupIc,
-  NaverLoginIc,
-  QnaToogleOpenIc,
-  QnaToogleCloseIc,
-  DownArrowGreyIc,
-  UpArrowGreyIc,
-  BasicPlanIc,
-  PremiumPlanIc,
-  CheckSmallIc,
-  ShowExplanationTagIc,
-  ShowQuestionTagIc,
-  MembershipDisabledIc,
-  KakaoIc,
-  XIc,
-  DisplayIc,
-  PrintIc,
-  FilledCircleIc,
-  EmptyCircleIc,
-  RadioButtonDefaultIc,
-  RadioButtonCheckIc,
-  CheckCircleBtnIc,
-  DownloadCircleIc,
-  PrintCircleIc,
-  RecorrectionBlueIc,
-  RecorrectionGrayIc,
   Advan1Ic,
   Advan2Ic,
   Advan3Ic,
   Advan4Ic,
   Advan5Ic,
+  BorderIc,
+  BasicPlanIc,
+  CheckBtnIc,
+  CheckCircleBtnIc,
+  CheckSmallIc,
+  CloseIc,
+  CouponIc,
+  CheckEmptyIc,
+  DiscountIc,
+  DisplayIc,
+  DownArrowBoldIc,
+  DownArrowGreyIc,
+  DownArrowIc,
+  DownArrowWhiteIc,
+  DownloadCircleIc,
+  EmptyCircleIc,
+  EmptyIc,
+  FilledCircleIc,
+  FindTestIc,
+  FooterLogoIc,
+  HomeTestUnsetIc,
+  InstaIc,
+  KakaoIc,
+  LeftArrowBigIc,
+  LeftArrowBlackBtn,
+  LeftArrowWhiteIc,
+  LeftCircleIc,
+  LeftCircleLongIc,
+  LoginInfoIc,
+  LogoIc,
+  MembershipDisabledIc,
+  MembershipIc,
+  NaverLoginIc,
+  NotCheckBtnIc,
+  PlusIc,
+  PracticeActiveIc,
+  PracticeDisabledIc,
+  PracticeIc,
+  PrecautionNumOneIc,
+  PrecautionNumThreeIc,
+  PrecautionNumTwoIc,
   PresentIc,
+  PrintCircleIc,
+  PrintIc,
+  PremiumPlanIc,
+  QnaToogleCloseIc,
+  QnaToogleOpenIc,
+  RadioButtonCheckIc,
+  RadioButtonDefaultIc,
+  RecorrectionBlueIc,
+  RecorrectionGrayIc,
+  RightArrowBigIc,
+  RightArrowBlueIc,
+  RightArrowIc,
+  RightArrowWhiteIc,
+  RightCircleIc,
+  RightWhiteArrowIc,
+  SearchIc,
+  ShowExplanationTagIc,
+  ShowQuestionTagIc,
+  SignupIc,
+  StudyActiveIc,
+  StudyDisabledIc,
+  TakeTestIc,
+  TestActiveIc,
+  TestDisabledIc,
   Univ1Ic,
   Univ2Ic,
   Univ3Ic,
@@ -189,11 +196,9 @@ export {
   Univ7Ic,
   Univ8Ic,
   Univ9Ic,
-  FooterLogoIc,
-  DownArrowWhiteIc,
-  TakeTestIc,
-  PracticeIc,
-  PlusIc,
-  CloseIc,
-  CheckEmptyIc,
+  UpArrowBoldIc,
+  UpArrowGreyIc,
+  UpArrowIc,
+  UpCircleIc,
+  XIc,
 };
