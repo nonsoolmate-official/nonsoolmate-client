eef415c99532e1be2cb2567bdf5ecc1c4362e99d
namdaeun <nde40345@gmail.com>
2024-10-12 02:07:20 +0900

    chore : 안 쓰이는 코드 제거

--- nonsoolmateClient/src/hooks/useModal.ts
+++ nonsoolmateClient/src/hooks/useModal.ts
@@ -1,14 +1,6 @@
-import { ModalDispatchContext, ModalStateContext } from "contexts/ModalProvider";
+import { ModalDispatchContext } from "contexts/ModalProvider";
 
 import { useContext } from "react";
-
-export function useModalState() {
-  const context = useContext(ModalStateContext);
-
-  if (!context) throw new Error("useModalState는 ModalProvider 안에서만 사용할 수 있습니다");
-
-  return context;
-}
 
 export function useModalDispatch() {
   const context = useContext(ModalDispatchContext);
