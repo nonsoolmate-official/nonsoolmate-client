ce28c7ab4b754cce3db2d4b5719d16f228a76e49
Soyoung Park <soyoung008@naver.com>
2024-10-24 18:26:22 +0900

    feat : 헤더에서 nonsoolmate 아이콘 클릭시 로그인 시에는 home/test, 미로그인 시에는 / 경로로 이동

--- src/pages/home/components/HomeHeader.tsx
+++ src/pages/home/components/HomeHeader.tsx
@@ -30,7 +30,7 @@ export default function HomeHeader() {
   return (
     <>
       <Header>
-        <LogoButton onClick={() => navigate("/")} type="button">
+        <LogoButton onClick={() => (getNameResponse ? navigate("/home/test") : navigate("/"))} type="button">
           <LogoIcon />
         </LogoButton>
         <HeaderInfo>
