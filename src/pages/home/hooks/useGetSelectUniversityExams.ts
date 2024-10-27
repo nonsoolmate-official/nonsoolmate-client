2c0fcb073bb8a3fb4d6c20adde68016cd75643ac
Rose <120544840+Rose-my@users.noreply.github.com>
2024-10-27 17:47:20 +0900

    feat : refetchInterval 삭제

--- src/pages/home/hooks/useGetSelectUniversityExams.ts
+++ src/pages/home/hooks/useGetSelectUniversityExams.ts
@@ -5,7 +5,7 @@ export default function useGetSelectUniversityExams() {
   const { data, refetch } = useQuery({
     queryKey: ["getSelectUniversityExams"],
     queryFn: () => getSelectUniversityExams(),
-    refetchInterval: 3000, // 3초마다 데이터를 refetch
+    // refetchInterval: 3000, // 3초마다 데이터를 refetch
   });
 
   return { data, refetch };
