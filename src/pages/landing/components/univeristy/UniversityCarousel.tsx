65241f57bdc8d5cca368fe4aa82093114cd8ce0a
namdaeun <nde40345@gmail.com>
2024-11-02 02:09:19 +0900

    style : 이미지 변경 및 추가

--- src/pages/landing/components/univeristy/UniversityCarousel.tsx
+++ src/pages/landing/components/univeristy/UniversityCarousel.tsx
@@ -1,12 +1,15 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import uni1 from "@assets/image/uni1.png";
 import uni2 from "@assets/image/uni2.png";
 import uni3 from "@assets/image/uni3.png";
 import uni4 from "@assets/image/uni4.png";
 import uni5 from "@assets/image/uni5.png";
 import uni6 from "@assets/image/uni6.png";
+import uni7 from "@assets/image/uni7.png";
+import uni8 from "@assets/image/uni8.png";
+import uni9 from "@assets/image/uni9.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 import styled from "styled-components";
 
 export default function UniversityCarousel() {
@@ -27,12 +30,15 @@ export default function UniversityCarousel() {
   return (
     <SliderContainer>
       <Slider {...settings}>
-        <Image src={uni1} alt="대학교 1" />
-        <Image src={uni2} alt="대학교 2" />
-        <Image src={uni3} alt="대학교 3" />
-        <Image src={uni4} alt="대학교 4" />
-        <Image src={uni5} alt="대학교 5" />
-        <Image src={uni6} alt="대학교 6" />
+        <Image src={uni1} alt="한양대학교" />
+        <Image src={uni2} alt="경희대학교" />
+        <Image src={uni3} alt="성균관대학교" />
+        <Image src={uni4} alt="이화여자대학교" />
+        <Image src={uni5} alt="중앙대학교" />
+        <Image src={uni6} alt="서강대학교" />
+        <Image src={uni7} alt="세종대학교" />
+        <Image src={uni8} alt="동국대학교" />
+        <Image src={uni9} alt="한국외국어대학교" />
       </Slider>
     </SliderContainer>
   );
