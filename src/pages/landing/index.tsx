ba6cc4d5c7341abc7622c1ce2fe706792d47ce4c
Rose <120544840+Rose-my@users.noreply.github.com>
2024-11-16 15:35:43 +0900

    design : revert overflow : hidden auto

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -44,7 +44,7 @@ export default function Landing() {
 // height: 100vh;
 
 const LandingContainer = styled.section<{ $isMobileSize: boolean }>`
-  overflow: ${({ $isMobileSize }) => ($isMobileSize ? "hidden" : "hidden auto")};
+  overflow: hidden auto;
   position: relative;
   height: ${({ $isMobileSize }) => ($isMobileSize ? "fit-content" : "100vh")};
 
