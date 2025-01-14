77bac2783f019f67ddfd6fea4055f15f2ac9a628
Rose <120544840+Rose-my@users.noreply.github.com>
2025-01-14 21:31:08 +0900

    core : table.ts

---
+++ src/pages/assign/constants/table.ts
@@ -0,0 +1,68 @@
+export const TEST_TABLE = {
+  COLUMN: {
+    NAME: "학생 이름",
+    MEMBERSHIP: "멤버십",
+    TEACHER: "담당강사",
+    TEST: "시험명",
+    ASSIGN: "배정",
+    TIME: "제출일시",
+    ALERT: "확정 및 알림 전송",
+  },
+  ROWS: [
+    {
+      NAME: "강아지",
+      MEMBERSHIP: "베이직",
+      TEACHER: "오소리",
+      TEST: "건국대학교 2023 인문1",
+      ASSIGN: "선택",
+      TIME: "10/01 23:59",
+      ALERT: "확인",
+    },
+    {
+      NAME: "카리나",
+      MEMBERSHIP: "프리미엄",
+      TEACHER: "이해찬",
+      TEST: "건국대학교 2023 인문1",
+      ASSIGN: "선택",
+      TIME: "10/01 23:59",
+      ALERT: "확인",
+    },
+  ],
+};
+
+export const STUDENT_TABLE = {
+  COLUMN: {
+    NAME: "학생 이름",
+    MEMBERSHIP: "멤버십",
+    DATE: "가입일",
+    COLLEGE: "목표대학",
+    ASSIGN: "배정",
+    ALERT: "확정",
+  },
+  ROWS: [
+    {
+      NAME: "강아지",
+      MEMBERSHIP: "베이직",
+      DATE: "2024-12-02",
+      COLLEGE: "성균관대학교, 이화여자대학교, 중앙대학교 ",
+      ASSIGN: "선택",
+      ALERT: "확인",
+    },
+    {
+      NAME: "이해찬",
+      MEMBERSHIP: "프리미엄",
+      DATE: "2024-12-02",
+      COLLEGE: "성균관대학교, 이화여자대학교, 중앙대학교, 엔시티대학교 ",
+      ASSIGN: "선택",
+      ALERT: "확인",
+    },
+  ],
+};
+
+export const ASSIGN = [
+  { ID: 1, NAME: "류가은", UNI: "건국/홍익" },
+  { ID: 2, NAME: "홍길동", UNI: "건국/홍익" },
+  { ID: 3, NAME: "이윤주", UNI: "건국/홍익" },
+  { ID: 4, NAME: "카리나", UNI: "건국/홍익" },
+  { ID: 5, NAME: "홍설", UNI: "서울/연세/홍익" },
+];
